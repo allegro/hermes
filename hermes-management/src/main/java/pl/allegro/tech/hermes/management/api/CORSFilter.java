fdba00ce2b57c5b073057918b16a4f9d123f79a7
Adam Dubiel <adam.dubiel@allegrogroup.com>
2015-05-19 12:10:43 +0200

    fixing #19 - open management api

---
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/CORSFilter.java
@@ -0,0 +1,25 @@
+package pl.allegro.tech.hermes.management.api;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Methods", "POST,PUT,GET,HEAD,DELETE");
+        headers.add("Access-Control-Max-Age", "1209600");
+        headers.addAll(
+                "Access-Control-Allow-Headers",
+                "X-Requested-With",
+                "Content-Type",
+                "Accept",
+                "Origin"
+        );
+    }
+}
