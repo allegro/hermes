95bb88a0cacef3af2e05704b8b9b95ea844d54ed
pawel.szymczyk <pawel.szymczyk@allegro.pl>
2015-06-02 15:18:38 +0200

    fixing #41 optional

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/message/tracker/MongoLogRepository.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/message/tracker/MongoLogRepository.java
@@ -78,7 +78,7 @@ public class MongoLogRepository implements LogRepository, LogSchemaAware {
                 .append(MESSAGE_ID, message.getId().get())
                 .append(CREATED_AT, clock.getDate())
                 .append(TIMESTAMP, timestamp)
-                .append(PUBLISH_TIMESTAMP, message.getPublishingTimestamp())
+                .append(PUBLISH_TIMESTAMP, message.getPublishingTimestamp().orElseGet(null))
                 .append(TOPIC_NAME, topicName)
                 .append(SUBSCRIPTION, subscriptionName)
                 .append(PARTITION, message.getPartition())
