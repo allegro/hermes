18d9f1ce0e0668769c09eaf636f1542796acc69b
Jakub Narloch <jakub.narloch@allegrogroup.com>
2015-11-03 17:11:46 +0100

    Added equals and hash code methods

--- hermes-api/src/main/java/pl/allegro/tech/hermes/api/TraceInfo.java
+++ hermes-api/src/main/java/pl/allegro/tech/hermes/api/TraceInfo.java
@@ -3,6 +3,8 @@ package pl.allegro.tech.hermes.api;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 public class TraceInfo {
 
     private final String traceId;
@@ -14,5 +16,21 @@ public class TraceInfo {
 
     public String getTraceId() {
         return traceId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TraceInfo traceInfo = (TraceInfo) o;
+
+        return !(traceId != null ? !traceId.equals(traceInfo.traceId) : traceInfo.traceId != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(traceId);
     }
 }
