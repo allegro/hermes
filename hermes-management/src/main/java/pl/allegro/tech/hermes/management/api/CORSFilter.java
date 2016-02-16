fbcd2ab9b9aa7e6bf3f76103e5512833e6d15dd5
Adam Dubiel <adam.dubiel@allegrogroup.com>
2016-02-16 11:41:47 +0100

    allowing Authorization header by default in management

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/CORSFilter.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/CORSFilter.java
@@ -21,7 +21,8 @@ public class CORSFilter implements ContainerResponseFilter {
                 "X-Requested-With",
                 "Content-Type",
                 "Accept",
-                "Origin"
+                "Origin",
+                "Authorization"
         );
     }
 }
