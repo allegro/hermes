1545e7d26e32094d522f674a5e4a6ee08dd580fd
Karol Lassak <karol.lassak@allegrogroup.com>
2016-02-26 12:24:28 +0100

    Added Graphite to Vagrant image
    - Closes #325

---
+++ vagrant_provisioning/graphite.pp
@@ -0,0 +1,3 @@
+class { 'graphite':
+  gr_apache_port  => 8082
+}
--- vagrant_provisioning/init.sh
+++ vagrant_provisioning/init.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+add-apt-repository ppa:openjdk-r/ppa 2>/dev/null
 apt-get -y update
-apt-get -qq install curl zip unzip jq git openjdk-8-jdk
+apt-get -qq install curl zip unzip jq git openjdk-8-jdk python-pip
 
 source /vagrant/vagrant_provisioning/install_supervisord.sh
 source /vagrant/vagrant_provisioning/install_kafka.sh
+source /vagrant/vagrant_provisioning/install_puppet_modules.sh
