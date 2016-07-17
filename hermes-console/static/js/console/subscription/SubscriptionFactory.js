04a11d714e2683eb160b3681c89b210b7772dd2a
Adam Dubiel <adam.dubiel@allegrogroup.com>
2016-07-17 21:29:38 +0200

    add ability to define subscription and topic defaults in console

---
+++ hermes-console/static/js/console/subscription/SubscriptionFactory.js
@@ -0,0 +1,31 @@
+var subscriptions = angular.module('hermes.subscription.factory', []);
+
+subscriptions.factory('SubscriptionFactory', ['SUBSCRIPTION_CONFIG', function (subscriptionConfig) {
+        return {
+            create: function (topicName) {
+                var defaults = {
+                    topicName: topicName,
+                    name: '',
+                    endpoint: '',
+                    description: '',
+                    supportTeam: '',
+                    contact: '',
+                    deliveryType: 'SERIAL',
+                    mode: 'ANYCAST',
+                    contentType: 'JSON',
+                    subscriptionPolicy: {
+                        messageTtl: 3600,
+                        messageBackoff: 100,
+                    },
+                    monitoringDetails: {
+                        severity: 'NON_IMPORTANT',
+                        reaction: ''
+                    }
+                };
+
+                _.merge(defaults, subscriptionConfig.defaults);
+                return defaults;
+            }
+        };
+
+    }]);
