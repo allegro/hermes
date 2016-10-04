fa96efede8d3f9bbf161ef2294d74ffcf9320b61
Daniel Krawczyk <dankraw@gmail.com>
2016-10-04 10:44:43 +0200

    Spring / Jersey upgrade (#592)

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/HermesManagement.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/HermesManagement.java
@@ -2,22 +2,22 @@ package pl.allegro.tech.hermes.management;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import pl.allegro.tech.hermes.management.config.SupportTeamServiceProperties;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan("pl.allegro.tech.hermes.management")
+@SpringBootApplication
 @EnableConfigurationProperties(SupportTeamServiceProperties.class)
 public class HermesManagement {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesManagement.class, args);
+    }
 
     @Bean(name = "managementRequestFactory")
     @ConfigurationProperties(prefix = "management.restTemplate")
@@ -28,9 +28,5 @@ public class HermesManagement {
     @Bean
     public RestTemplate restTemplate(@Qualifier("managementRequestFactory") ClientHttpRequestFactory clientHttpRequestFactory) {
         return new RestTemplate(clientHttpRequestFactory);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(HermesManagement.class, args);
     }
 }
