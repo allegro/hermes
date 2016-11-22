42579752f0c92daec43e9fd4a02546e652533457
Lukasz Druminski <lukasz.druminski@allegrogroup.com>
2016-11-22 10:58:11 +0100

    #503 Changing priorities values for startup/shutdown hooks

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/hook/Hook.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/hook/Hook.java
@@ -2,8 +2,8 @@ package pl.allegro.tech.hermes.common.hook;
 
 public interface Hook {
     int LOWER_PRIORITY = 0;
-    int NORMAL_PRIORITY = 1;
-    int HIGHER_PRIORITY = 2;
+    int NORMAL_PRIORITY = 100;
+    int HIGHER_PRIORITY = 200;
 
     void apply();
 
