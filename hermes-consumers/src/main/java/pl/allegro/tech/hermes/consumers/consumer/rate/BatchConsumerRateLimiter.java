4a3ff8b2613e9b32d4080e4a38e85d847249d897
Adam Dubiel <adam.dubiel@allegrogroup.com>
2017-01-20 12:55:32 +0100

    Decouple filtering rate limiting from backpressure based rate lmiting

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/rate/BatchConsumerRateLimiter.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/rate/BatchConsumerRateLimiter.java
@@ -17,6 +17,10 @@ public class BatchConsumerRateLimiter implements ConsumerRateLimiter {
     }
 
     @Override
+    public void acquireFiltered() {
+    }
+
+    @Override
     public void adjustConsumerRate() {
     }
 
