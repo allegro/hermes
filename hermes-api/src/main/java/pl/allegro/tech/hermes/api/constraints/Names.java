9e9535e39bb28d659168c1e7c2b8026e7a4b0b66
Przemyslaw Piorkowski <piorkowskiprzemyslaw@gmail.com>
2017-09-28 01:11:00 +0200

    Adds special chars restriction in group, topic and subscription name (#802)

--- hermes-api/src/main/java/pl/allegro/tech/hermes/api/constraints/Names.java
+++ hermes-api/src/main/java/pl/allegro/tech/hermes/api/constraints/Names.java
@@ -1,5 +1,16 @@
 package pl.allegro.tech.hermes.api.constraints;
 
 public final class Names {
-    public static final String ALLOWED_NAME_REGEX = "[a-zA-Z0-9_.-]+";
+    private static final String ALLOWED_CHARACTERS = "[a-zA-Z0-9_]";
+    private static final String DELIMITER = "\\.";
+
+    public static final String ALLOWED_NAME_REGEX = ALLOWED_CHARACTERS + "+";
+
+    public static final String ALLOWED_GROUP_NAME_REGEX =
+            ALLOWED_NAME_REGEX + "(" + DELIMITER + ALLOWED_NAME_REGEX + ")*";
+
+    public static final String ALLOWED_TOPIC_NAME_REGEX = ALLOWED_NAME_REGEX;
+
+    public static final String ALLOWED_SUBSCRIPTION_NAME_REGEX = ALLOWED_NAME_REGEX;
+
 }
