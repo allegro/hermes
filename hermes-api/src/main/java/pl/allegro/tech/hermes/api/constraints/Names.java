a721e4f01d42ebc55dbf52d712293decac56b7ca
Adam Dubiel <adam.dubiel@allegrogroup.com>
2017-10-24 18:09:06 +0200

    Revert "Special chars restriction in group, topic and subscription name"
    This reverts commit 1846c493f6377355cf89eacc542123c1d16c5601.

--- hermes-api/src/main/java/pl/allegro/tech/hermes/api/constraints/Names.java
+++ hermes-api/src/main/java/pl/allegro/tech/hermes/api/constraints/Names.java
@@ -1,18 +1,5 @@
 package pl.allegro.tech.hermes.api.constraints;
 
 public final class Names {
-    private static final String ALLOWED_CHARACTERS = "[a-zA-Z0-9_]";
-    private static final String DELIMITER = "\\.";
-
-    public static final String ALLOWED_NAME_REGEX = ALLOWED_CHARACTERS + "+";
-
-    public static final String ALLOWED_NAME_WITH_DELIMITER_REGEX =
-            ALLOWED_NAME_REGEX + "(" + DELIMITER + ALLOWED_NAME_REGEX + ")*";
-
-    public static final String ALLOWED_GROUP_NAME_REGEX = ALLOWED_NAME_WITH_DELIMITER_REGEX;
-
-    public static final String ALLOWED_TOPIC_NAME_REGEX = ALLOWED_NAME_REGEX;
-
-    public static final String ALLOWED_SUBSCRIPTION_NAME_REGEX = ALLOWED_NAME_WITH_DELIMITER_REGEX;
-
+    public static final String ALLOWED_NAME_REGEX = "[a-zA-Z0-9_.-]+";
 }
