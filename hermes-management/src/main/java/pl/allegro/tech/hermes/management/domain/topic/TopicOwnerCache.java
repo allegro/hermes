563e8167f54e8fceb458f39eae15c645c57d5f46
Jakub Dyszkiewicz <jakub.dyszkiewicz@allegro.pl>
2018-07-04 11:26:50 +0200

    Refreshing topic owner cache every 5 minutes

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/topic/TopicOwnerCache.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/topic/TopicOwnerCache.java
@@ -3,8 +3,10 @@ package pl.allegro.tech.hermes.management.domain.topic;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.allegro.tech.hermes.api.OwnerId;
 import pl.allegro.tech.hermes.api.Topic;
@@ -12,8 +14,11 @@ import pl.allegro.tech.hermes.api.TopicName;
 import pl.allegro.tech.hermes.domain.topic.TopicRepository;
 import pl.allegro.tech.hermes.management.domain.group.GroupService;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Collection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class TopicOwnerCache {
@@ -22,20 +27,31 @@ public class TopicOwnerCache {
 
     private final TopicRepository topicRepository;
     private final GroupService groupService;
+    private final ScheduledExecutorService scheduledExecutorService;
 
     private final Multimap<OwnerId, TopicName> cache = Multimaps.synchronizedMultimap(ArrayListMultimap.create());
 
-    public TopicOwnerCache(TopicRepository topicRepository, GroupService groupService) {
+    public TopicOwnerCache(TopicRepository topicRepository, GroupService groupService,
+                           @Value("${topicOwnerCache.refreshRateInSeconds}") int refreshRateInSeconds) {
         this.topicRepository = topicRepository;
         this.groupService = groupService;
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(
+                new ThreadFactoryBuilder()
+                        .setNameFormat("topic-owner-cache-%d")
+                        .build());
+        scheduledExecutorService.scheduleAtFixedRate(this::fillCache, 0, refreshRateInSeconds, TimeUnit.SECONDS);
+    }
+
+    @PreDestroy
+    public void stop() {
+        scheduledExecutorService.shutdown();
     }
 
     public Collection<TopicName> get(OwnerId ownerId) {
         return cache.get(ownerId);
     }
 
-    @PostConstruct
-    public void fillCache() {
+    private void fillCache() {
         logger.info("Starting filling Owner Id to Topic Name cache");
         long start = System.currentTimeMillis();
         groupService.listGroupNames().stream()
@@ -45,15 +61,15 @@ public class TopicOwnerCache {
         logger.info("Cache filled. Took {}ms", end - start);
     }
 
-    public void onRemovedTopic(Topic topic) {
+    void onRemovedTopic(Topic topic) {
         cache.remove(topic.getOwner(), topic.getName());
     }
 
-    public void onCreatedTopic(Topic topic) {
+    void onCreatedTopic(Topic topic) {
         cache.put(topic.getOwner(), topic.getName());
     }
 
-    public void onUpdatedTopic(Topic oldTopic, Topic newTopic) {
+    void onUpdatedTopic(Topic oldTopic, Topic newTopic) {
         cache.remove(oldTopic.getOwner(), oldTopic.getName());
         cache.put(newTopic.getOwner(), newTopic.getName());
     }
