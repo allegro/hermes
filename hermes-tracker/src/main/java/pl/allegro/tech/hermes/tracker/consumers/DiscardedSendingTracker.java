3864a4789d135f9f0fea0ea1d4a45395ad08fea9
Adrian Kunowski <adrian.kunowski@gmail.com>
2018-10-16 14:57:09 +0200

    Trace only discarded messages (#918)

---
+++ hermes-tracker/src/main/java/pl/allegro/tech/hermes/tracker/consumers/DiscardedSendingTracker.java
@@ -0,0 +1,45 @@
+package pl.allegro.tech.hermes.tracker.consumers;
+
+import java.time.Clock;
+import java.util.List;
+
+public class DiscardedSendingTracker implements SendingTracker {
+
+
+    private final List<LogRepository> repositories;
+    private final Clock clock;
+
+    DiscardedSendingTracker(List<LogRepository> repositories, Clock clock) {
+        this.repositories = repositories;
+        this.clock = clock;
+    }
+
+    @Override
+    public void logSent(MessageMetadata message, String hostname) {
+
+    }
+
+    @Override
+    public void logFailed(MessageMetadata message, String reason, String hostname) {
+    }
+
+    @Override
+    public void logDiscarded(MessageMetadata message, String reason) {
+        repositories.forEach(r ->
+                r.logDiscarded(message, clock.millis(), reason));
+    }
+
+    @Override
+    public void logInflight(MessageMetadata message) {
+
+    }
+
+    @Override
+    public void logFiltered(MessageMetadata messageMetadata, String reason) {
+
+    }
+
+    void add(LogRepository logRepository) {
+        repositories.add(logRepository);
+    }
+}
