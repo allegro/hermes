a07676b110cca214ba31cc41545f43666e71d159
Jacek Ewertowski <jacek.ewertowski@allegro.pl>
2019-07-04 11:44:33 +0200

    Auto switching to read only mode in hermes-management #1052 - fix port already in use

--- hermes-management/src/test/groovy/pl/allegro/tech/hermes/management/utils/MultiZookeeperIntegrationTest.groovy
+++ hermes-management/src/test/groovy/pl/allegro/tech/hermes/management/utils/MultiZookeeperIntegrationTest.groovy
@@ -3,13 +3,14 @@ package pl.allegro.tech.hermes.management.utils
 import org.apache.curator.test.TestingServer
 import pl.allegro.tech.hermes.management.infrastructure.dc.DatacenterNameProvider
 import pl.allegro.tech.hermes.management.infrastructure.zookeeper.ZookeeperClient
+import pl.allegro.tech.hermes.test.helper.util.Ports
 import spock.lang.Specification
 
 abstract class MultiZookeeperIntegrationTest extends Specification {
 
-    static final int DC_1_ZOOKEEPER_PORT = 9500
+    static final int DC_1_ZOOKEEPER_PORT = Ports.nextAvailable()
     static final String DC_1_NAME = "dc1"
-    static final int DC_2_ZOOKEEPER_PORT = 9501
+    static final int DC_2_ZOOKEEPER_PORT = Ports.nextAvailable()
     static final String DC_2_NAME = "dc2"
 
     TestingServer zookeeper1
