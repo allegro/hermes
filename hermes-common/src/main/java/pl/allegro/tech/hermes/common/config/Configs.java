df3d5774215d25a578a3e3b5d80ff2ce7e909d12
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2019-08-22 14:59:37 +0200

    resolve #1082 | Add topic metadata refresh job

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/config/Configs.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/config/Configs.java
@@ -148,6 +148,9 @@ public enum Configs {
     FRONTEND_STARTUP_TOPIC_METADATA_LOADING_RETRY_COUNT("frontend.startup.topic.metadata.loading.retry.count", 5),
     FRONTEND_STARTUP_TOPIC_METADATA_LOADING_THREAD_POOL_SIZE("frontend.startup.topic.metadata.loading.thread.pool.size", 16),
 
+    FRONTEND_TOPIC_METADATA_LOADING_JOB_ENABLED("frontend.startup.topic.metadata.job.enabled", false),
+    FRONTEND_TOPIC_METADATA_LOADING_JOB_INTERVAL("frontend.startup.topic.metadata.job.interval", 60),
+
     FRONTEND_STARTUP_TOPIC_SCHEMA_LOADING_ENABLED("frontend.startup.topic.schema.loading.enabled", false),
     FRONTEND_STARTUP_TOPIC_SCHEMA_LOADING_RETRY_COUNT("frontend.startup.topic.schema.loading.retry.count", 3),
     FRONTEND_STARTUP_TOPIC_SCHEMA_LOADING_THREAD_POOL_SIZE("frontend.startup.topic.schema.loading.thread.pool.size", 16),
---
+++ hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/server/TopicMetadataLoadingJob.java
@@ -0,0 +1,53 @@
+package pl.allegro.tech.hermes.frontend.server;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import pl.allegro.tech.hermes.common.config.ConfigFactory;
+import pl.allegro.tech.hermes.common.config.Configs;
+
+import javax.inject.Inject;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+public class TopicMetadataLoadingJob implements Runnable {
+
+    private final TopicMetadataLoadingRunner topicMetadataLoadingRunner;
+    private final ScheduledExecutorService executorService;
+    private final int intervalSeconds;
+
+    private ScheduledFuture job;
+
+    @Inject
+    public TopicMetadataLoadingJob(TopicMetadataLoadingRunner topicMetadataLoadingRunner,
+                                   ConfigFactory config) {
+        this(topicMetadataLoadingRunner, config.getIntProperty(Configs.FRONTEND_TOPIC_METADATA_LOADING_JOB_INTERVAL));
+    }
+
+    TopicMetadataLoadingJob(TopicMetadataLoadingRunner topicMetadataLoadingRunner, int intervalSeconds) {
+        this.topicMetadataLoadingRunner = topicMetadataLoadingRunner;
+        this.intervalSeconds = intervalSeconds;
+
+        ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("TopicMetadataLoadingExecutor-%d").build();
+        this.executorService = Executors.newSingleThreadScheduledExecutor(threadFactory);
+    }
+
+    @Override
+    public void run() {
+        topicMetadataLoadingRunner.refreshMetadata();
+    }
+
+    public void start() {
+        job = executorService.scheduleAtFixedRate(this, intervalSeconds, intervalSeconds, TimeUnit.SECONDS);
+    }
+
+    public void stop() throws InterruptedException {
+        job.cancel(false);
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+    }
+
+
+}
