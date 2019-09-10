bbce7c622b721ea16356bf5bc1d7a12e4f1c1657
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2019-09-10 12:58:19 +0200

    hermes-management supports workload constraints (#1091)

---
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/domain/workload/constraints/SubscriptionConstraintsDoNotExistException.java
@@ -0,0 +1,19 @@
+package pl.allegro.tech.hermes.domain.workload.constraints;
+
+import pl.allegro.tech.hermes.api.ErrorCode;
+import pl.allegro.tech.hermes.api.SubscriptionName;
+import pl.allegro.tech.hermes.common.exception.HermesException;
+
+import static pl.allegro.tech.hermes.api.ErrorCode.SUBSCRIPTION_CONSTRAINTS_DO_NOT_EXIST;
+
+public class SubscriptionConstraintsDoNotExistException extends HermesException {
+
+    public SubscriptionConstraintsDoNotExistException(SubscriptionName subscriptionName, Throwable cause) {
+        super(String.format("Constraints for subscription %s do not exist.", subscriptionName.getQualifiedName()), cause);
+    }
+
+    @Override
+    public ErrorCode getCode() {
+        return SUBSCRIPTION_CONSTRAINTS_DO_NOT_EXIST;
+    }
+}
--- hermes-common/src/test/groovy/pl/allegro/tech/hermes/infrastructure/zookeeper/ZookeeperWorkloadConstraintsRepositoryTest.groovy
+++ hermes-common/src/test/groovy/pl/allegro/tech/hermes/infrastructure/zookeeper/ZookeeperWorkloadConstraintsRepositoryTest.groovy
@@ -1,9 +1,14 @@
 package pl.allegro.tech.hermes.infrastructure.zookeeper
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.zookeeper.KeeperException
 import pl.allegro.tech.hermes.api.SubscriptionName
 import pl.allegro.tech.hermes.api.TopicName
 import pl.allegro.tech.hermes.api.Constraints
+import pl.allegro.tech.hermes.domain.workload.constraints.SubscriptionConstraintsAlreadyExistException
+import pl.allegro.tech.hermes.domain.workload.constraints.SubscriptionConstraintsDoNotExistException
+import pl.allegro.tech.hermes.domain.workload.constraints.TopicConstraintsAlreadyExistException
+import pl.allegro.tech.hermes.domain.workload.constraints.TopicConstraintsDoNotExistException
 import pl.allegro.tech.hermes.test.IntegrationTest
 
 import java.util.concurrent.TimeUnit
@@ -46,9 +51,178 @@ class ZookeeperWorkloadConstraintsRepositoryTest extends IntegrationTest {
         constraints.subscriptionConstraints.get(SubscriptionName.fromString('group.topic$sub')).consumersNumber == 3
     }
 
+    def "should create constraints for topic"() {
+        given:
+        def topic = TopicName.fromQualifiedName('group.topic')
+
+        when:
+        repository.createConstraints(topic, new Constraints(1))
+        wait.untilWorkloadConstraintsCreated(topic)
+
+        then:
+        assertNodeContainsData('/hermes/consumers-workload-constraints/group.topic', new Constraints(1))
+    }
+
+    def "should throw exception if topic node already exist"() {
+        given:
+        def topic = TopicName.fromQualifiedName('group.topic')
+
+        when:
+        repository.createConstraints(topic, new Constraints(1))
+        wait.untilWorkloadConstraintsCreated(topic)
+        repository.createConstraints(topic, new Constraints(1))
+
+        then:
+        def e = thrown(TopicConstraintsAlreadyExistException)
+        e.message == "Constraints for topic group.topic already exist."
+        e.cause instanceof KeeperException.NodeExistsException
+    }
+
+    def "should create constraints for subscription"() {
+        given:
+        def subscription = SubscriptionName.fromString('group.topic$sub')
+
+        when:
+        repository.createConstraints(subscription, new Constraints(1))
+        wait.untilWorkloadConstraintsCreated(subscription)
+
+        then:
+        assertNodeContainsData('/hermes/consumers-workload-constraints/group.topic$sub', new Constraints(1))
+    }
+
+    def "should throw exception if subscription node already exist"() {
+        given:
+        def subscription = SubscriptionName.fromString('group.topic$sub')
+
+        when:
+        repository.createConstraints(subscription, new Constraints(1))
+        wait.untilWorkloadConstraintsCreated(subscription)
+        repository.createConstraints(subscription, new Constraints(1))
+
+        then:
+        def e = thrown(SubscriptionConstraintsAlreadyExistException)
+        e.message == 'Constraints for subscription group.topic$sub already exist.'
+        e.cause instanceof KeeperException.NodeExistsException
+    }
+
+    def "should update topic constraints"() {
+        given:
+        setupNode('/hermes/consumers-workload-constraints/group.topic', new Constraints(1))
+        ensureCacheWasUpdated(1)
+
+        when:
+        repository.updateConstraints(TopicName.fromQualifiedName('group.topic'), new Constraints(2))
+
+        then:
+        assertNodeContainsData('/hermes/consumers-workload-constraints/group.topic', new Constraints(2))
+    }
+
+    def "should throw exception if topic node does not exist on update"() {
+        given:
+        def topic = TopicName.fromQualifiedName('group.topic')
+
+        when:
+        repository.updateConstraints(topic, new Constraints(1))
+
+        then:
+        def e = thrown(TopicConstraintsDoNotExistException)
+        e.message == "Constraints for topic group.topic do not exist."
+        e.cause instanceof KeeperException.NoNodeException
+    }
+
+    def "should update subscription constraints"() {
+        given:
+        setupNode('/hermes/consumers-workload-constraints/group.topic$sub', new Constraints(1))
+        ensureCacheWasUpdated(1)
+
+        when:
+        repository.updateConstraints(SubscriptionName.fromString('group.topic$sub'), new Constraints(2))
+
+        then:
+        assertNodeContainsData('/hermes/consumers-workload-constraints/group.topic$sub', new Constraints(2))
+    }
+
+    def "should throw exception if subscription node does not exist on update"() {
+        when:
+        repository.updateConstraints(SubscriptionName.fromString('group.topic$sub'), new Constraints(1))
+
+        then:
+        def e = thrown(SubscriptionConstraintsDoNotExistException)
+        e.message == 'Constraints for subscription group.topic$sub do not exist.'
+        e.cause instanceof KeeperException.NoNodeException
+    }
+
+    def "should delete topic constraints"() {
+        given:
+        setupNode('/hermes/consumers-workload-constraints/group.topic', new Constraints(1))
+        ensureCacheWasUpdated(1)
+
+        when:
+        repository.deleteConstraints(TopicName.fromQualifiedName('group.topic'))
+
+        then:
+        assertNodeDoesNotExists('/hermes/consumers-workload-constraints/group.topic')
+    }
+
+    def "should delete subscription constraints"() {
+        given:
+        setupNode('/hermes/consumers-workload-constraints/group.topic$sub', new Constraints(1))
+        ensureCacheWasUpdated(1)
+
+        when:
+        repository.deleteConstraints(SubscriptionName.fromString('group.topic$sub'))
+
+        then:
+        assertNodeDoesNotExists('/hermes/consumers-workload-constraints/group.topic$sub')
+    }
+
+    def "should ignore not existing node"() {
+        when:
+        repository.deleteConstraints(TopicName.fromQualifiedName('group.topic'))
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "should return true if topic constraints exist"() {
+        given:
+        setupNode('/hermes/consumers-workload-constraints/group.topic', new Constraints(1))
+        ensureCacheWasUpdated(1)
+
+        expect:
+        repository.constraintsExist(TopicName.fromQualifiedName('group.topic'))
+    }
+
+    def "should return false if topic constraints do not exist"() {
+        expect:
+        !repository.constraintsExist(TopicName.fromQualifiedName('group.topic'))
+    }
+
+    def "should return true if subscription constraints exist"() {
+        given:
+        setupNode('/hermes/consumers-workload-constraints/group.topic$sub', new Constraints(1))
+        ensureCacheWasUpdated(1)
+
+        expect:
+        repository.constraintsExist(SubscriptionName.fromString('group.topic$sub'))
+    }
+
+    def "should return false if subscription constraints do not exist"() {
+        expect:
+        !repository.constraintsExist(SubscriptionName.fromString('group.topic$sub'))
+    }
+
     private def ensureCacheWasUpdated(int expectedSize) {
         await()
                 .atMost(200, TimeUnit.MILLISECONDS)
                 .until { cache.getCurrentData().size() == expectedSize }
+    }
+
+    private def assertNodeContainsData(String path, Constraints constraints) {
+        zookeeper().getData().forPath(path) == objectMapper.writeValueAsBytes(constraints)
+    }
+
+    private def assertNodeDoesNotExists(String path) {
+        zookeeper().checkExists().forPath(path) == null
     }
 }
