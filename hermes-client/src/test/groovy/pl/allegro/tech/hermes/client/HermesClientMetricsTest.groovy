3acede571c7e337891d4ca5537e03f89eddc3090
Robert Duda <robert.duda@allegro.pl>
2019-09-16 13:34:44 +0200

    resolve #1009 | new test cases

--- hermes-client/src/test/groovy/pl/allegro/tech/hermes/client/HermesClientMetricsTest.groovy
+++ hermes-client/src/test/groovy/pl/allegro/tech/hermes/client/HermesClientMetricsTest.groovy
@@ -68,22 +68,33 @@ class HermesClientMetricsTest extends Specification {
 
     def "should update retries metrics"() {
         given:
-        def retries = 3
-        HermesClient client = hermesClient(failingHermesSender(retries - 1))
+        HermesClient client1 = hermesClient(failingHermesSender(2))
                 .withRetrySleep(0)
-                .withRetries(retries)
+                .withRetries(4)
+                .withMetrics(metricsProvider).build()
+
+        HermesClient client2 = hermesClient(failingHermesSender(5))
+                .withRetrySleep(0)
+                .withRetries(6)
+                .withMetrics(metricsProvider).build()
+
+        HermesClient client3 = hermesClient(failingHermesSender(3))
+                .withRetrySleep(0)
+                .withRetries(2)
                 .withMetrics(metricsProvider).build()
 
         when:
-        silence({ client.publish("com.group.topic", "123").join() })
+        silence({ client1.publish("com.group.topic", "123").join() })
+        silence({ client2.publish("com.group.topic", "456").join() })
+        silence({ client3.publish("com.group.topic", "789").join() })
 
         then:
-        metrics.counter("hermes-client.com_group.topic.failure").count == 2
-        metrics.counter("hermes-client.com_group.topic.retries.exhausted").count == 0
-        metrics.counter("hermes-client.com_group.topic.retries.success").count == 1
-        metrics.counter("hermes-client.com_group.topic.retries.count").count == 2
+        metrics.counter("hermes-client.com_group.topic.failure").count == 10
+        metrics.counter("hermes-client.com_group.topic.retries.exhausted").count == 1
+        metrics.counter("hermes-client.com_group.topic.retries.success").count == 2
+        metrics.counter("hermes-client.com_group.topic.retries.count").count == 9
         metrics.histogram("hermes-client.com_group.topic.retries.attempts").getSnapshot().getMin() == 2
-        metrics.histogram("hermes-client.com_group.topic.retries.attempts").getSnapshot().getMax() == 2
+        metrics.histogram("hermes-client.com_group.topic.retries.attempts").getSnapshot().getMax() == 5
         metrics.timers.containsKey("hermes-client.com_group.topic.latency")
     }
 
