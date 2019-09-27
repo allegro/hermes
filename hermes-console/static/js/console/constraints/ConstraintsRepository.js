352f9ee4c17ba12208f57d97c7025b207de632d6
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2019-09-27 11:06:18 +0200

    Remove unnecessary hasOwnProperty check.

--- hermes-console/static/js/console/constraints/ConstraintsRepository.js
+++ hermes-console/static/js/console/constraints/ConstraintsRepository.js
@@ -11,22 +11,18 @@ repository.factory('ConstraintsRepository', ['DiscoveryService', '$resource',
                         var workloadConstraints = {};
                         workloadConstraints.topicConstraints = [];
                         for (var topicName in response.topicConstraints) {
-                            if (response.topicConstraints.hasOwnProperty(topicName)) {
-                                workloadConstraints.topicConstraints.push({
-                                    topicName: topicName,
-                                    consumersNumber: response.topicConstraints[topicName].consumersNumber
-                                });
-                            }
+                            workloadConstraints.topicConstraints.push({
+                                topicName: topicName,
+                                consumersNumber: response.topicConstraints[topicName].consumersNumber
+                            });
                         }
 
                         workloadConstraints.subscriptionConstraints = [];
                         for (var subscriptionName in response.subscriptionConstraints) {
-                            if (response.subscriptionConstraints.hasOwnProperty(subscriptionName)) {
-                                workloadConstraints.subscriptionConstraints.push({
-                                    subscriptionName: subscriptionName,
-                                    consumersNumber: response.subscriptionConstraints[subscriptionName].consumersNumber
-                                });
-                            }
+                            workloadConstraints.subscriptionConstraints.push({
+                                subscriptionName: subscriptionName,
+                                consumersNumber: response.subscriptionConstraints[subscriptionName].consumersNumber
+                            });
                         }
                         return workloadConstraints;
                     })
