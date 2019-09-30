ce442a4e74bc7705c0bead81b1253f4f35b66053
Daniel Krawczyk <daniel.krawczyk@allegro.pl>
2019-09-30 11:59:44 +0200

    Increment schema version for workload, modify decoder check

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/supervisor/workload/ConsumerWorkloadDecoder.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/supervisor/workload/ConsumerWorkloadDecoder.java
@@ -1,16 +1,22 @@
 package pl.allegro.tech.hermes.consumers.supervisor.workload;
 
 import org.agrona.concurrent.UnsafeBuffer;
+import org.slf4j.Logger;
 import pl.allegro.tech.hermes.api.SubscriptionName;
 import pl.allegro.tech.hermes.consumers.subscription.id.SubscriptionId;
 import pl.allegro.tech.hermes.consumers.subscription.id.SubscriptionIds;
 import pl.allegro.tech.hermes.consumers.supervisor.workload.sbe.stubs.AssignmentsDecoder;
 import pl.allegro.tech.hermes.consumers.supervisor.workload.sbe.stubs.MessageHeaderDecoder;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 class ConsumerWorkloadDecoder {
+
+    private static final Logger logger = getLogger(ConsumerWorkloadDecoder.class);
 
     private final SubscriptionIds subscriptionIds;
 
@@ -25,9 +31,13 @@ class ConsumerWorkloadDecoder {
         UnsafeBuffer buffer = new UnsafeBuffer(data);
         header.wrap(buffer, 0);
 
-        if (header.templateId() != AssignmentsDecoder.TEMPLATE_ID) {
-            throw new IllegalStateException(String.format("AssignmentsDecoder TEMPLATE_ID=%d does not match encoded TEMPLATE_ID=%d",
-                    AssignmentsDecoder.TEMPLATE_ID, header.templateId()));
+        if (header.schemaId() != AssignmentsDecoder.SCHEMA_ID || header.templateId() != AssignmentsDecoder.TEMPLATE_ID) {
+            logger.warn("Unable to decode assignments, schema or template id mismatch. " +
+                    "Required by decoder: [schema id={}, template id={}], " +
+                    "encoded in payload: [schema id={}, template id={}]",
+                    AssignmentsDecoder.SCHEMA_ID, AssignmentsDecoder.TEMPLATE_ID,
+                    header.schemaId(), header.templateId());
+            return Collections.emptySet();
         }
         body.wrap(buffer, header.encodedLength(), header.blockLength(), header.version());
 
