ea9d2994b0c035717d7ceb9655036f995a04a83d
Oleg Kovalov <oleg.kovalov@allegro.pl>
2019-10-08 13:17:42 +0200

    Add new role

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/auth/Roles.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/auth/Roles.java
@@ -1,8 +1,9 @@
 package pl.allegro.tech.hermes.management.api.auth;
 
 public final class Roles {
-    public static final String SUBSCRIPTION_OWNER = "subscriptionOwner";
-    public static final String TOPIC_OWNER = "topicOwner";
-    public static final String ADMIN = "admin";
     public static final String ANY = "any";
+    public static final String ADMIN = "admin";
+    public static final String RESOURCE_WITHOUT_OWNER = "resourceWithoutOwner";
+    public static final String TOPIC_OWNER = "topicOwner";
+    public static final String SUBSCRIPTION_OWNER = "subscriptionOwner";
 }
