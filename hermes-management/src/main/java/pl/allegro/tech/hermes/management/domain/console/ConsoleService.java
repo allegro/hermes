5c89b4abfed4cc62e679db5899c17d4250168c19
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2020-01-31 16:54:12 +0100

    Hermes Console served by Hermes Management as static resource (allegro#994) (#1107)

---
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/console/ConsoleService.java
@@ -0,0 +1,17 @@
+package pl.allegro.tech.hermes.management.domain.console;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsoleService {
+
+    private ConsoleConfigurationRepository repository;
+
+    public ConsoleService(ConsoleConfigurationRepository repository) {
+        this.repository = repository;
+    }
+
+    public String getConfiguration() {
+        return "var config = " + repository.find();
+    }
+}
