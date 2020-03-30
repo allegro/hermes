70b5acbd7ddfa6805c399d358d3f37dd380cffb1
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2020-03-30 22:01:08 +0200

    Validate __metadata field in avro schema (#1015)

--- hermes-management/src/test/groovy/pl/allegro/tech/hermes/management/infrastructure/schema/validator/AvroSchemaValidatorTest.groovy
+++ hermes-management/src/test/groovy/pl/allegro/tech/hermes/management/infrastructure/schema/validator/AvroSchemaValidatorTest.groovy
@@ -10,7 +10,7 @@ class AvroSchemaValidatorTest extends Specification {
 
     def "should accept valid schema"() {
         given:
-        def schema = IOUtils.toString(getClass().getResourceAsStream("/schema/user.avsc"), "UTF-8")
+        def schema = readSchema("/schema/user.avsc")
 
         expect:
         avroSchemaValidator.check(schema)
@@ -30,12 +30,48 @@ class AvroSchemaValidatorTest extends Specification {
         thrown(InvalidSchemaException)
     }
 
-
     def "should throw exception for invalid json"() {
         when:
         avroSchemaValidator.check('{')
 
         then:
         thrown(InvalidSchemaException)
+    }
+
+    def "should throw exception in case of empty schema"() {
+        when:
+        avroSchemaValidator.check("")
+
+        then:
+        def e = thrown IllegalArgumentException
+        e.message == "Message schema cannot be empty"
+    }
+
+    def "should throw exception in case of lack of __metadata field"() {
+        given:
+        def schema = readSchema("/schema/user_no_metadata.avsc")
+
+        when:
+        avroSchemaValidator.check(schema)
+
+        then:
+        def e = thrown InvalidSchemaException
+        e.message == "Error while trying to validate schema: Missing Hermes __metadata field"
+    }
+
+    def "should throw exception in case of incorrect types used in __metadata field"() {
+        given:
+        def schema = readSchema("/schema/user_metadata_with_avro_java_string.avsc")
+
+        when:
+        avroSchemaValidator.check(schema)
+
+        then:
+        def e = thrown InvalidSchemaException
+        e.message == "Error while trying to validate schema: Invalid types used in field __metadata"
+    }
+
+    private String readSchema(String path) {
+        IOUtils.toString(getClass().getResourceAsStream(path), "UTF-8")
     }
 }
