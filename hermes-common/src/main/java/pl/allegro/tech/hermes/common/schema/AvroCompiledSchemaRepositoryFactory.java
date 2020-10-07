4cdd7f63e3f4b861140467f4889b8e487b0ca3b3
Łukasz Drumiński <lukasz.druminski@allegro.pl>
2020-10-07 22:29:00 +0200

    Resolves #1257 | A topic change updates its all cached schemas (#1270)

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/schema/AvroCompiledSchemaRepositoryFactory.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/schema/AvroCompiledSchemaRepositoryFactory.java
@@ -2,76 +2,43 @@ package pl.allegro.tech.hermes.common.schema;
 
 import org.apache.avro.Schema;
 import org.glassfish.hk2.api.Factory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import pl.allegro.tech.hermes.api.ContentType;
-import pl.allegro.tech.hermes.api.Topic;
 import pl.allegro.tech.hermes.common.config.ConfigFactory;
 import pl.allegro.tech.hermes.common.config.Configs;
-import pl.allegro.tech.hermes.domain.notifications.InternalNotificationsBus;
-import pl.allegro.tech.hermes.domain.notifications.TopicCallback;
 import pl.allegro.tech.hermes.schema.CachedCompiledSchemaRepository;
 import pl.allegro.tech.hermes.schema.CompiledSchemaRepository;
 import pl.allegro.tech.hermes.schema.DirectCompiledSchemaRepository;
 import pl.allegro.tech.hermes.schema.RawSchemaClient;
 import pl.allegro.tech.hermes.schema.SchemaCompilersFactory;
-import pl.allegro.tech.hermes.schema.SchemaVersionsRepository;
 
 import javax.inject.Inject;
 
-public class AvroCompiledSchemaRepositoryFactory implements Factory<CompiledSchemaRepository<Schema>> {
+import static pl.allegro.tech.hermes.common.config.Configs.SCHEMA_CACHE_COMPILED_MAXIMUM_SIZE;
+import static pl.allegro.tech.hermes.common.config.Configs.SCHEMA_CACHE_ENABLED;
 
-    private static final Logger logger = LoggerFactory.getLogger(AvroCompiledSchemaRepositoryFactory.class);
+public class AvroCompiledSchemaRepositoryFactory implements Factory<CompiledSchemaRepository<Schema>> {
 
     private final RawSchemaClient rawSchemaClient;
     private final ConfigFactory configFactory;
-    private final InternalNotificationsBus notificationsBus;
-    private final SchemaVersionsRepository schemaVersionsRepository;
 
     @Inject
     public AvroCompiledSchemaRepositoryFactory(RawSchemaClient rawSchemaClient,
-                                               ConfigFactory configFactory,
-                                               InternalNotificationsBus notificationsBus,
-                                               SchemaVersionsRepository schemaVersionsRepository) {
+                                               ConfigFactory configFactory) {
         this.rawSchemaClient = rawSchemaClient;
         this.configFactory = configFactory;
-        this.notificationsBus = notificationsBus;
-        this.schemaVersionsRepository = schemaVersionsRepository;
     }
 
     @Override
     public CompiledSchemaRepository<Schema> provide() {
-        CachedCompiledSchemaRepository<Schema> repository = new CachedCompiledSchemaRepository<>(
-                new DirectCompiledSchemaRepository<>(rawSchemaClient, SchemaCompilersFactory.avroSchemaCompiler()),
-                configFactory.getIntProperty(Configs.SCHEMA_CACHE_COMPILED_MAXIMUM_SIZE),
-                configFactory.getIntProperty(Configs.SCHEMA_CACHE_COMPILED_EXPIRE_AFTER_ACCESS_MINUTES));
+        CompiledSchemaRepository<Schema> repository = new DirectCompiledSchemaRepository<>(rawSchemaClient,
+                SchemaCompilersFactory.avroSchemaCompiler());
 
-        notificationsBus.registerTopicCallback(new TopicCallback() {
-            @Override
-            public void onTopicRemoved(Topic topic) {
-                repository.removeFromCache(topic);
-            }
-
-            @Override
-            public void onTopicCreated(Topic topic) {
-                updateLatestSchema(topic, "new");
-            }
-
-            @Override
-            public void onTopicChanged(Topic topic) {
-                updateLatestSchema(topic, "updated");
-            }
-
-            private void updateLatestSchema(Topic topic, String operationName) {
-                if (topic.getContentType() == ContentType.AVRO) {
-                    logger.info("Loading latest schema for {} topic {}", operationName, topic.getQualifiedName());
-                    schemaVersionsRepository.onlineLatestSchemaVersion(topic).ifPresent(
-                            schemaVersion -> repository.getSchema(topic, schemaVersion, true));
-                }
-            }
-        });
-
-        return repository;
+        if (configFactory.getBooleanProperty(SCHEMA_CACHE_ENABLED)) {
+            return new CachedCompiledSchemaRepository<>(repository,
+                    configFactory.getIntProperty(SCHEMA_CACHE_COMPILED_MAXIMUM_SIZE),
+                    configFactory.getIntProperty(Configs.SCHEMA_CACHE_COMPILED_EXPIRE_AFTER_ACCESS_MINUTES));
+        } else {
+            return repository;
+        }
     }
 
     @Override
---
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/schema/SchemaCacheRefresherCallback.java
@@ -0,0 +1,67 @@
+package pl.allegro.tech.hermes.common.schema;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.allegro.tech.hermes.api.ContentType;
+import pl.allegro.tech.hermes.api.Topic;
+import pl.allegro.tech.hermes.domain.notifications.TopicCallback;
+import pl.allegro.tech.hermes.schema.CachedCompiledSchemaRepository;
+import pl.allegro.tech.hermes.schema.CachedSchemaVersionsRepository;
+import pl.allegro.tech.hermes.schema.CouldNotLoadSchemaException;
+import pl.allegro.tech.hermes.schema.SchemaVersion;
+import pl.allegro.tech.hermes.schema.SchemaVersionsResult;
+
+import java.util.List;
+
+class SchemaCacheRefresherCallback implements TopicCallback {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchemaVersionsRepositoryFactory.class);
+
+    public static final boolean REFRESH_ONLINE = true;
+
+    private final CachedSchemaVersionsRepository schemaVersionsRepository;
+    private final CachedCompiledSchemaRepository compiledSchemaRepository;
+
+    public SchemaCacheRefresherCallback(CachedSchemaVersionsRepository schemaVersionsRepository,
+                                        CachedCompiledSchemaRepository compiledSchemaRepository) {
+        this.schemaVersionsRepository = schemaVersionsRepository;
+        this.compiledSchemaRepository = compiledSchemaRepository;
+    }
+
+    @Override
+    public void onTopicRemoved(Topic topic) {
+        schemaVersionsRepository.removeFromCache(topic);
+        compiledSchemaRepository.removeFromCache(topic);
+    }
+
+    @Override
+    public void onTopicCreated(Topic topic) {
+        refreshSchemas(topic);
+    }
+
+    @Override
+    public void onTopicChanged(Topic topic) {
+        refreshSchemas(topic);
+    }
+
+    private void refreshSchemas(Topic topic) {
+        if (topic.getContentType() == ContentType.AVRO) {
+            logger.info("Refreshing all schemas for {} topic.", topic.getQualifiedName());
+            SchemaVersionsResult versions = schemaVersionsRepository.versions(topic, REFRESH_ONLINE);
+            if (versions.isSuccess()) {
+                refreshCompiledSchemas(topic, versions.get());
+            }
+        }
+    }
+
+    private void refreshCompiledSchemas(Topic topic, List<SchemaVersion> schemaVersions) {
+        schemaVersions.forEach(schemaVersion -> {
+            try {
+                compiledSchemaRepository.getSchema(topic, schemaVersion, REFRESH_ONLINE);
+            } catch (CouldNotLoadSchemaException e) {
+                logger.warn("Schema for topic {} at version {} could not be loaded",
+                        topic.getQualifiedName(), schemaVersion.value(), e);
+            }
+        });
+    }
+}
