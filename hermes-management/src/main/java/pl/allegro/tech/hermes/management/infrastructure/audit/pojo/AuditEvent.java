a9d5339cfc36adb56a9be74f9c87aced88fa88b9
Dysp <c.desc2@gmail.com>
2020-10-31 13:00:32 +0100

    Add more info to audit events

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/infrastructure/audit/pojo/AuditEvent.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/infrastructure/audit/pojo/AuditEvent.java
@@ -2,13 +2,21 @@ package pl.allegro.tech.hermes.management.infrastructure.audit.pojo;
 
 public class AuditEvent {
     private final AuditEventType eventType;
+    private final Class<?> resourceClass;
     private final Class<?> payloadClass;
     private final Object payload;
+    private final String username;
 
-    public AuditEvent(AuditEventType eventType, Object payload) {
+    public AuditEvent(AuditEventType eventType, Object payload, String username) {
+        this(eventType, payload, payload.getClass(), username);
+    }
+
+    public AuditEvent(AuditEventType eventType, Object payload, Class<?> resourceClass, String username){
         this.eventType = eventType;
         this.payload = payload;
         this.payloadClass = payload.getClass();
+        this.resourceClass = resourceClass;
+        this.username = username;
     }
 
     public AuditEventType getEventType() {
@@ -21,5 +29,13 @@ public class AuditEvent {
 
     public Object getPayload() {
         return payload;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Class<?> getResourceClass() {
+        return resourceClass;
     }
 }
