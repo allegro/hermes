41790c4d227f397da86dd0fba2ee603537b49069
Piotr Rżysko <piotr.rzysko@allegro.pl>
2021-02-22 11:39:31 +0100

    Fix failing test: KafkaConsumerGroupManagerSpec (#1321)

--- hermes-management/src/test/groovy/pl/allegro/tech/hermes/management/infrastructure/kafka/service/KafkaConsumerGroupManagerSpec.groovy
+++ hermes-management/src/test/groovy/pl/allegro/tech/hermes/management/infrastructure/kafka/service/KafkaConsumerGroupManagerSpec.groovy
@@ -7,7 +7,7 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.TopicPartition
 import org.junit.Rule
-import org.springframework.boot.test.rule.OutputCapture
+import org.springframework.boot.test.system.OutputCaptureRule
 import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.spock.Testcontainers
@@ -35,7 +35,7 @@ import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_
 class KafkaConsumerGroupManagerSpec extends Specification {
 
     @Rule
-    OutputCapture output = new OutputCapture()
+    OutputCaptureRule output = new OutputCaptureRule()
 
     @Shared
     KafkaContainer kafkaContainer = new KafkaContainer()
