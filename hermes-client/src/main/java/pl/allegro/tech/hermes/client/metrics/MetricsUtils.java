1a49d0f3e0376da9517c4c6d04195e2a5f54d160
mateusz.szczygiel <mateusz.szczygiel@allegro.pl>
2021-07-20 10:57:35 +0200

    Topic name and metric key type are now published in tags.

--- hermes-client/src/main/java/pl/allegro/tech/hermes/client/metrics/MetricsUtils.java
+++ hermes-client/src/main/java/pl/allegro/tech/hermes/client/metrics/MetricsUtils.java
@@ -2,11 +2,7 @@ package pl.allegro.tech.hermes.client.metrics;
 
 class MetricsUtils {
 
-    static String getMetricsPrefix(String topic) {
-        return "hermes-client." + sanitizeTopic(topic);
-    }
-
-    private static String sanitizeTopic(String topic) {
+    static String sanitizeTopic(String topic) {
         int lastDot = topic.lastIndexOf(".");
         char[] sanitized = topic.replaceAll("\\.", "_").toCharArray();
         sanitized[lastDot] = '.';
