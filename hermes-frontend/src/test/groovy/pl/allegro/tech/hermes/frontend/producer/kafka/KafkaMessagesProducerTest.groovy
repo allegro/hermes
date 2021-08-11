58fdad091711cefb7e122517d3bece92ede7091e
yevgeniya.li <yevgeniya.li@allegro.pl>
2021-08-11 15:57:09 +0200

    test

--- hermes-frontend/src/test/groovy/pl/allegro/tech/hermes/frontend/producer/kafka/KafkaMessagesProducerTest.groovy
+++ hermes-frontend/src/test/groovy/pl/allegro/tech/hermes/frontend/producer/kafka/KafkaMessagesProducerTest.groovy
@@ -17,7 +17,7 @@ class KafkaMessagesProducerTest extends Specification {
     @Shared
     CachedTopic cachedTopic = cachedTopic("group.test")
 
-    def "should successfully publish messages if publishing single message on broker is successful"() {
+    def "should successfully publish messages if publishing any single message on broker is successful"() {
         given:
         List<Message> jsonMessages = (0..<4).collect {jsonMessage(it)}
         BrokerMessageProducer brokerMessageProducer = TestBrokerMessageProducer.builder().throwException(false).build()
