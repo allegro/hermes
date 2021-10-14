a82c2c424558306b37731b1ac01d3d47ef20472e
Michal Bobowski <michal.bobowski@allegro.pl>
2021-10-14 16:13:49 +0200

    Fixing $resource bug on getting mode info (#1384)

--- hermes-console/static/js/console/Mode.js
+++ hermes-console/static/js/console/Mode.js
@@ -1,15 +1,13 @@
 var modeModule = angular.module('hermes.mode', []);
 
-modeModule.factory('Mode', ['DiscoveryService', '$resource', '$rootScope',
-    function (discovery, $resource, $rootScope) {
-
-        var modeResource = $resource(discovery.resolve('/mode'));
-
+modeModule.factory('Mode', ['DiscoveryService', '$http', '$rootScope',
+    function (discovery, $http, $rootScope) {
         return {
             reload: function () {
-                modeResource.get().$promise.then(
-                    function (value) {
-                        $rootScope.readOnly = value === 'readOnly';
+                // intentionally use $http over $resource because of https://stackoverflow.com/questions/24876593/resource-query-return-split-strings-array-of-char-instead-of-a-string
+                $http.get(discovery.resolve('/mode')).then(
+                    function (response) {
+                        $rootScope.readOnly = (response.data === 'readOnly' || response.data === 'readOnlyAdmin');
                     });
             }
         };
