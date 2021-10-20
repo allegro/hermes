bc3584b3d9b37aefabf1767b261cb0a96f88afe2
Dawid Urbanowski <dawid.urbanowski@allegro.pl>
2021-10-20 12:35:57 +0200

    basic version of working console

--- hermes-console/static/js/console/consistency/ConsistencyController.js
+++ hermes-console/static/js/console/consistency/ConsistencyController.js
@@ -16,12 +16,26 @@ consistency.controller('ConsistencyController', ['$scope', '$state', 'toaster', 
             result: consistencyRepository.getLastConsistencyCheckingResult()
         };
 
+        $scope.topicsConsistencyChecking = {
+            state: consistencyCheckingStates.READY,
+            result: consistencyRepository.getLastTopicsConsistencyCheckingResult()
+        }
+
         $scope.$watch('consistencyChecking.result', function (result) {
             consistencyRepository.setLastConsistencyCheckingResult(result);
         }, true);
 
-        $scope.checkConsistency = function () {
-            setState(consistencyCheckingStates.CHECKING_CONSISTENCY);
+        $scope.$watch('topicsConsistencyChecking.result', function (result) {
+            consistencyRepository.setLastTopicsConsistencyCheckingResult(result);
+        }, true);
+
+        $scope.checkConsistency = function() {
+            checkGroupConsistency()
+            findTopicsNotPresentInHermes()
+        }
+
+        function checkGroupConsistency() {
+            setGroupsState(consistencyCheckingStates.CHECKING_CONSISTENCY);
             setInconsistentGroups(null);
             $scope.processedGroupsPercent = 0;
             $scope.processedGroups = 0;
@@ -48,9 +62,20 @@ consistency.controller('ConsistencyController', ['$scope', '$state', 'toaster', 
                     setInconsistentGroups(null);
                 })
                 .finally(function () {
-                    setState(consistencyCheckingStates.READY);
+                    setGroupsState(consistencyCheckingStates.READY);
                 });
-        };
+        }
+
+        function findTopicsNotPresentInHermes() {
+            setTopicsState(consistencyCheckingStates.CHECKING_CONSISTENCY)
+            consistencyRepository.listInconsistentTopics()
+            .then(function (topics) {
+                setInconsistentTopics(topics)
+            })
+            .finally(function () {
+                setTopicsState(consistencyCheckingStates.READY);
+            });
+        }
 
         function showErrorPopup(message, response) {
             toaster.pop(
@@ -60,13 +85,23 @@ consistency.controller('ConsistencyController', ['$scope', '$state', 'toaster', 
             );
         }
 
-        function setState(inconsistentGroups) {
+        function setGroupsState(inconsistentGroups) {
             $scope.consistencyChecking.state = inconsistentGroups;
+        }
+
+        function setTopicsState(inconsistentGroups) {
+            $scope.topicsConsistencyChecking.state = inconsistentGroups;
         }
 
         function setInconsistentGroups(inconsistentGroups) {
             $scope.consistencyChecking.result = {
                 inconsistentGroups: inconsistentGroups
+            };
+        }
+
+        function setInconsistentTopics(inconsistentTopics) {
+            $scope.topicsConsistencyChecking.result = {
+                inconsistentTopics: inconsistentTopics
             };
         }
 
