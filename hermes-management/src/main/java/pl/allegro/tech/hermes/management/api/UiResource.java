7d9df10d69e189154ae90ccc433bad16b3d8291f
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2021-11-03 09:14:15 +0100

    [hermes-management] restore text/html content-type for web pages (#1413)

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/UiResource.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/UiResource.java
@@ -4,10 +4,14 @@ import org.glassfish.jersey.server.mvc.Viewable;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.TEXT_HTML;
 
 @Path("/")
 public class UiResource {
     @GET
+    @Produces(TEXT_HTML)
     public Viewable getIndex() {
         return new Viewable("/index.html");
     }
