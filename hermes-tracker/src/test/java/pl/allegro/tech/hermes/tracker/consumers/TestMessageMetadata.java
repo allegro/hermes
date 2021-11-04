4192c359ee9d182868cc3aadc69132ed7376cb4d
Michal Ciszewski <michal.ciszewski@allegro.pl>
2021-11-04 12:56:51 +0100

    Correct constructing test message metadata

--- hermes-tracker/src/test/java/pl/allegro/tech/hermes/tracker/consumers/TestMessageMetadata.java
+++ hermes-tracker/src/test/java/pl/allegro/tech/hermes/tracker/consumers/TestMessageMetadata.java
@@ -1,5 +1,8 @@
 package pl.allegro.tech.hermes.tracker.consumers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestMessageMetadata {
 
     public static MessageMetadata of(String messageId, String topic, String subscription) {
@@ -7,7 +10,7 @@ public class TestMessageMetadata {
     }
 
     public static MessageMetadata of(String messageId, String topic, String subscription, long offset, int partition) {
-        return new MessageMetadata(messageId, offset, partition, 123L, topic, subscription, topic, 123456L, 123456L);
+        return new MessageMetadata(messageId, offset, partition, 123L, topic, subscription, topic, 123456L, 123456L, sampleExtraHeaders());
     }
 
     public static MessageMetadata of(String messageId,String batchId, String topic, String subscription) {
@@ -15,6 +18,13 @@ public class TestMessageMetadata {
     }
 
     public static MessageMetadata of(String messageId, String batchId, String topic, String subscription, long offset, int partition) {
-        return new MessageMetadata(messageId, batchId, offset, partition, 123L, topic, subscription, topic, 123456L, 123456L);
+        return new MessageMetadata(messageId, batchId, offset, partition, 123L, topic, subscription, topic, 123456L, 123456L, sampleExtraHeaders());
+    }
+
+    private static Map<String, String> sampleExtraHeaders() {
+        Map<String, String> sampleHeaders = new HashMap<>();
+        sampleHeaders.put("trace-id", "uuid-1");
+        sampleHeaders.put("span-id", "uuid-2");
+        return sampleHeaders;
     }
 }
