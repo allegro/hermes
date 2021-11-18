6f48e0437cf19965b153c8052d8df283e0830abd
Piotr Rżysko <piotr.rzysko@allegro.pl>
2021-11-18 15:05:25 +0100

    Log backup storage size on shutdown (#1422)

---
+++ hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/buffer/chronicle/ChronicleMapClosedException.java
@@ -0,0 +1,8 @@
+package pl.allegro.tech.hermes.frontend.buffer.chronicle;
+
+public class ChronicleMapClosedException extends RuntimeException {
+
+    public ChronicleMapClosedException(String message) {
+        super(message);
+    }
+}
