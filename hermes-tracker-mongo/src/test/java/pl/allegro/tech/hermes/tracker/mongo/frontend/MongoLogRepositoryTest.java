0e0697da5b650064ccf2ce409a8e76c414d6bcd1
Michal Ciszewski <michal.ciszewski@allegro.pl>
2021-12-03 14:38:22 +0100

    Finish Mongo log backend tests

--- hermes-tracker-mongo/src/test/java/pl/allegro/tech/hermes/tracker/mongo/frontend/MongoLogRepositoryTest.java
+++ hermes-tracker-mongo/src/test/java/pl/allegro/tech/hermes/tracker/mongo/frontend/MongoLogRepositoryTest.java
@@ -2,18 +2,20 @@ package pl.allegro.tech.hermes.tracker.mongo.frontend;
 
 import com.codahale.metrics.MetricRegistry;
 import com.github.fakemongo.Fongo;
-import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import pl.allegro.tech.hermes.api.PublishedMessageTraceStatus;
+import pl.allegro.tech.hermes.metrics.PathsCompiler;
 import pl.allegro.tech.hermes.tracker.frontend.AbstractLogRepositoryTest;
 import pl.allegro.tech.hermes.tracker.frontend.LogRepository;
 import pl.allegro.tech.hermes.tracker.mongo.LogSchemaAware;
-import pl.allegro.tech.hermes.metrics.PathsCompiler;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static com.jayway.awaitility.Duration.ONE_SECOND;
+import static com.mongodb.QueryBuilder.start;
+import static java.util.regex.Pattern.compile;
+import static java.util.regex.Pattern.quote;
 
 public class MongoLogRepositoryTest extends AbstractLogRepositoryTest implements LogSchemaAware {
 
@@ -30,12 +32,7 @@ public class MongoLogRepositoryTest extends AbstractLogRepositoryTest implements
                                                 String remoteHostname,
                                                 PublishedMessageTraceStatus status,
                                                 String... extraRequestHeadersKeywords) throws Exception {
-        awaitUntilMessageIsPersisted(
-                new BasicDBObject(TOPIC_NAME, topic)
-                .append(STATUS, status.toString())
-                .append(MESSAGE_ID, id)
-                .append(REMOTE_HOSTNAME, remoteHostname)
-                .append(EXTRA_REQUEST_HEADERS, extraRequestHeadersKeywords));
+        awaitUntilMessageIsPersisted(constructExpectedLogQuery(topic, id, "host", status, extraRequestHeadersKeywords));
     }
 
     @Override
@@ -45,13 +42,7 @@ public class MongoLogRepositoryTest extends AbstractLogRepositoryTest implements
                                                 String remoteHostname,
                                                 PublishedMessageTraceStatus status,
                                                 String... extraRequestHeadersKeywords) throws Exception {
-        awaitUntilMessageIsPersisted(
-                new BasicDBObject(TOPIC_NAME, topic)
-                .append(STATUS, status.toString())
-                .append(MESSAGE_ID, id)
-                .append(REASON, reason)
-                .append(REMOTE_HOSTNAME, remoteHostname)
-                .append(EXTRA_REQUEST_HEADERS, extraRequestHeadersKeywords));
+        awaitUntilMessageIsPersisted(constructExpectedLogQuery(topic, id, "host", reason, status, extraRequestHeadersKeywords));
     }
 
     private void awaitUntilMessageIsPersisted(DBObject query) throws Exception {
@@ -61,4 +52,44 @@ public class MongoLogRepositoryTest extends AbstractLogRepositoryTest implements
         });
     }
 
+    private DBObject constructExpectedLogQuery(String topic,
+                                               String id,
+                                               String hostname,
+                                               String reason,
+                                               PublishedMessageTraceStatus status,
+                                               String... extraRequestHeadersKeywords) {
+        DBObject reasonCondition = start(REASON).is(reason).get();
+        DBObject remainingConditions = constructExpectedLogQuery(topic, id, hostname, status, extraRequestHeadersKeywords);
+        return start()
+            .and(remainingConditions, reasonCondition)
+            .get();
+    }
+
+    private DBObject constructExpectedLogQuery(String topic,
+                                               String id,
+                                               String hostname,
+                                               PublishedMessageTraceStatus status,
+                                               String... extraRequestHeadersKeywords) {
+        DBObject basicConditions =
+            start(TOPIC_NAME).is(topic)
+                .put(STATUS).is(status.toString())
+                .put(MESSAGE_ID).is(id)
+                .put(SOURCE_HOSTNAME).is(hostname)
+                .get();
+
+        return start()
+            .and(basicConditions)
+            .and(constructExtraRequestHeadersConditions(extraRequestHeadersKeywords))
+            .get();
+    }
+
+    private DBObject[] constructExtraRequestHeadersConditions(String... extraRequestHeadersKeywords) {
+        DBObject[] conditions = new DBObject[extraRequestHeadersKeywords.length];
+        for (int i = 0; i < extraRequestHeadersKeywords.length; ++i) {
+            conditions[i] = start(EXTRA_REQUEST_HEADERS)
+                .regex(compile(quote(extraRequestHeadersKeywords[i])))
+                .get();
+        }
+        return conditions;
+    }
 }
