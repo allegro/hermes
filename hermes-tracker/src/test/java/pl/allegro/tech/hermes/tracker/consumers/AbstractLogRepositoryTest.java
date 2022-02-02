61ab2cf7e0f45873f6402f870d1fdc1f7f227f4f
Szymon Marcinkiewicz <szymon.a.marcinkiewicz@allegro.pl>
2022-02-02 14:13:28 +0100

    Bumped testNg version to 7.4.0 - fixes

--- hermes-tracker/src/test/java/pl/allegro/tech/hermes/tracker/consumers/AbstractLogRepositoryTest.java
+++ hermes-tracker/src/test/java/pl/allegro/tech/hermes/tracker/consumers/AbstractLogRepositoryTest.java
@@ -24,7 +24,7 @@ public abstract class AbstractLogRepositoryTest {
     @BeforeSuite
     public void setUpRetry(ITestContext context) {
         for (ITestNGMethod method : context.getAllTestMethods()) {
-            method.setRetryAnalyzer(new Retry());
+            method.setRetryAnalyzerClass(Retry.class);
         }
     }
 
--- hermes-tracker/src/test/java/pl/allegro/tech/hermes/tracker/frontend/AbstractLogRepositoryTest.java
+++ hermes-tracker/src/test/java/pl/allegro/tech/hermes/tracker/frontend/AbstractLogRepositoryTest.java
@@ -23,7 +23,7 @@ public abstract class AbstractLogRepositoryTest {
     @BeforeSuite
     public void setUpRetry(ITestContext context) {
         for (ITestNGMethod method : context.getAllTestMethods()) {
-            method.setRetryAnalyzer(new Retry());
+            method.setRetryAnalyzerClass(Retry.class);
         }
     }
 
