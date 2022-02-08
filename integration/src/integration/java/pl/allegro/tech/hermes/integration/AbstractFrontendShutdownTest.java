f41097b9789f8a7c37ba0256169917b9cf93ebcf
Piotr Majorczyk <piotr.majorczyk@allegro.pl>
2022-02-08 16:06:34 +0100

    Changes in integration tests (WIP)

--- integration/src/integration/java/pl/allegro/tech/hermes/integration/AbstractFrontendShutdownTest.java
+++ integration/src/integration/java/pl/allegro/tech/hermes/integration/AbstractFrontendShutdownTest.java
@@ -1,53 +1,67 @@
 package pl.allegro.tech.hermes.integration;
 
+import org.springframework.context.ApplicationContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pl.allegro.tech.hermes.common.config.ConfigFactory;
 import pl.allegro.tech.hermes.common.config.Configs;
 import pl.allegro.tech.hermes.frontend.HermesFrontend;
 import pl.allegro.tech.hermes.frontend.server.HermesServer;
+import pl.allegro.tech.hermes.integration.env.FrontendStarter;
 import pl.allegro.tech.hermes.test.helper.config.MutableConfigFactory;
 import pl.allegro.tech.hermes.test.helper.endpoint.HermesPublisher;
 import pl.allegro.tech.hermes.test.helper.util.Ports;
 
-public abstract class AbstractFrontendShutdownTest extends IntegrationTest {
+    public abstract class AbstractFrontendShutdownTest extends IntegrationTest {
 
     public static final int FRONTEND_PORT = Ports.nextAvailable();
     public static final String FRONTEND_URL = "http://127.0.0.1:" + FRONTEND_PORT;
 
     protected HermesPublisher publisher;
     protected HermesServer hermesServer;
+    protected FrontendStarter frontendStarter;
 
-    private HermesFrontend hermesFrontend;
-
-    @BeforeClass
+        @BeforeClass
     public void setup() throws Exception {
-        ConfigFactory configFactory = new MutableConfigFactory()
-                .overrideProperty(Configs.FRONTEND_PORT, FRONTEND_PORT)
-                .overrideProperty(Configs.FRONTEND_SSL_ENABLED, false)
-                .overrideProperty(Configs.KAFKA_AUTHORIZATION_ENABLED, false)
-                .overrideProperty(Configs.KAFKA_BROKER_LIST, kafkaClusterOne.getBootstrapServersForExternalClients())
-                .overrideProperty(Configs.ZOOKEEPER_CONNECT_STRING, hermesZookeeperOne.getConnectionString())
-                .overrideProperty(Configs.SCHEMA_REPOSITORY_SERVER_URL, schemaRegistry.getUrl())
-                .overrideProperty(Configs.FRONTEND_GRACEFUL_SHUTDOWN_ENABLED, false)
-                .overrideProperty(Configs.MESSAGES_LOCAL_STORAGE_ENABLED, false);
+//        ConfigFactory configFactory = new MutableConfigFactory()
+//                .overrideProperty(Configs.FRONTEND_PORT, FRONTEND_PORT)
+//                .overrideProperty(Configs.FRONTEND_SSL_ENABLED, false)
+//                .overrideProperty(Configs.KAFKA_AUTHORIZATION_ENABLED, false)
+//                .overrideProperty(Configs.KAFKA_BROKER_LIST, kafkaClusterOne.getBootstrapServersForExternalClients())
+//                .overrideProperty(Configs.ZOOKEEPER_CONNECT_STRING, hermesZookeeperOne.getConnectionString())
+//                .overrideProperty(Configs.SCHEMA_REPOSITORY_SERVER_URL, schemaRegistry.getUrl())
+//                .overrideProperty(Configs.FRONTEND_GRACEFUL_SHUTDOWN_ENABLED, false)
+//                .overrideProperty(Configs.MESSAGES_LOCAL_STORAGE_ENABLED, false);
 
-        hermesFrontend = HermesFrontend.frontend()
-                .withBinding(configFactory, ConfigFactory.class)
-                .withDisabledGlobalShutdownHook()
-                .withDisabledFlushLogsShutdownHook()
-                .build();
+        frontendStarter = new FrontendStarter();
+        frontendStarter.setSpringProfiles("integration");
+        frontendStarter.overrideProperty(Configs.FRONTEND_PORT, FRONTEND_PORT);
+        frontendStarter.overrideProperty(Configs.FRONTEND_SSL_ENABLED, false);
+        frontendStarter.overrideProperty(Configs.KAFKA_AUTHORIZATION_ENABLED, false);
+        frontendStarter.overrideProperty(Configs.KAFKA_BROKER_LIST, kafkaClusterOne.getBootstrapServersForExternalClients());
+        frontendStarter.overrideProperty(Configs.ZOOKEEPER_CONNECT_STRING, hermesZookeeperOne.getConnectionString());
+        frontendStarter.overrideProperty(Configs.SCHEMA_REPOSITORY_SERVER_URL, schemaRegistry.getUrl());
+        frontendStarter.overrideProperty(Configs.FRONTEND_GRACEFUL_SHUTDOWN_ENABLED, false);
+        frontendStarter.overrideProperty(Configs.MESSAGES_LOCAL_STORAGE_ENABLED, false);
+        frontendStarter.start();
+            //    private HermesFrontend hermesFrontend;
+//        hermesFrontend = HermesFrontend.frontend()
+//                .withBinding(configFactory, ConfigFactory.class)
+//                .withDisabledGlobalShutdownHook()//TODO
+//                .withDisabledFlushLogsShutdownHook()//TODO
+//                .build();
 
-        hermesFrontend.start();
+//        hermesFrontend.start();
 
-        hermesServer = hermesFrontend.getService(HermesServer.class);
+        hermesServer = frontendStarter.instance().getBean(HermesServer.class);//TODO? - no IoC
 
         publisher = new HermesPublisher(FRONTEND_URL);
     }
 
     @AfterClass
-    public void tearDown() throws InterruptedException {
-        hermesFrontend.stop();
+    public void tearDown() throws Exception {
+//        hermesFrontend.stop();
+        frontendStarter.stop();
     }
 
 }
--- integration/src/integration/java/pl/allegro/tech/hermes/integration/CustomSslContextFactoryTest.java
+++ integration/src/integration/java/pl/allegro/tech/hermes/integration/CustomSslContextFactoryTest.java
@@ -9,46 +9,61 @@ import pl.allegro.tech.hermes.common.config.Configs;
 import pl.allegro.tech.hermes.common.ssl.SslContextFactory;
 import pl.allegro.tech.hermes.frontend.HermesFrontend;
 import pl.allegro.tech.hermes.frontend.server.SslContextFactoryProvider;
+import pl.allegro.tech.hermes.integration.env.FrontendStarter;
 import pl.allegro.tech.hermes.test.helper.config.MutableConfigFactory;
 import pl.allegro.tech.hermes.test.helper.util.Ports;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CustomSslContextFactoryTest extends IntegrationTest {
+public class CustomSslContextFactoryTest extends IntegrationTest { //TODO - do we need this test?
 
     public static final int FRONTEND_PORT = Ports.nextAvailable();
 
     private HermesFrontend hermesFrontend;
+    private FrontendStarter frontendStarter;
 
     @Test
-    public void shouldInjectCustomSslContextFactoryToFrontend() {
+    public void shouldInjectCustomSslContextFactoryToFrontend() throws Exception {
         // given
         SslContextFactory customSslContextFactory = Mockito.mock(SslContextFactory.class);
 
-        ConfigFactory configFactory = new MutableConfigFactory()
-                .overrideProperty(Configs.FRONTEND_PORT, FRONTEND_PORT)
-                .overrideProperty(Configs.FRONTEND_SSL_ENABLED, false)
-                .overrideProperty(Configs.KAFKA_AUTHORIZATION_ENABLED, false)
-                .overrideProperty(Configs.KAFKA_BROKER_LIST, kafkaClusterOne.getBootstrapServersForExternalClients())
-                .overrideProperty(Configs.ZOOKEEPER_CONNECT_STRING, hermesZookeeperOne.getConnectionString())
-                .overrideProperty(Configs.SCHEMA_REPOSITORY_SERVER_URL, schemaRegistry.getUrl())
-                .overrideProperty(Configs.MESSAGES_LOCAL_STORAGE_DIRECTORY, Files.createTempDir().getAbsolutePath());
+//        ConfigFactory configFactory = new MutableConfigFactory()
+//                .overrideProperty(Configs.FRONTEND_PORT, FRONTEND_PORT)
+//                .overrideProperty(Configs.FRONTEND_SSL_ENABLED, false)
+//                .overrideProperty(Configs.KAFKA_AUTHORIZATION_ENABLED, false)
+//                .overrideProperty(Configs.KAFKA_BROKER_LIST, kafkaClusterOne.getBootstrapServersForExternalClients())
+//                .overrideProperty(Configs.ZOOKEEPER_CONNECT_STRING, hermesZookeeperOne.getConnectionString())
+//                .overrideProperty(Configs.SCHEMA_REPOSITORY_SERVER_URL, schemaRegistry.getUrl())
+//                .overrideProperty(Configs.MESSAGES_LOCAL_STORAGE_DIRECTORY, Files.createTempDir().getAbsolutePath());
 
-        hermesFrontend = HermesFrontend.frontend()
-                .withBinding(configFactory, ConfigFactory.class)
-                .withSslContextFactory(customSslContextFactory)
-                .build();
-        hermesFrontend.start();
+        frontendStarter = new FrontendStarter();
+        frontendStarter.setSpringProfiles("shouldInjectCustomSslContextFactoryToFrontend");
+        frontendStarter.overrideProperty(Configs.FRONTEND_PORT, FRONTEND_PORT);
+        frontendStarter.overrideProperty(Configs.FRONTEND_SSL_ENABLED, false);
+        frontendStarter.overrideProperty(Configs.KAFKA_AUTHORIZATION_ENABLED, false);
+        frontendStarter.overrideProperty(Configs.KAFKA_BROKER_LIST, kafkaClusterOne.getBootstrapServersForExternalClients());
+        frontendStarter.overrideProperty(Configs.ZOOKEEPER_CONNECT_STRING, hermesZookeeperOne.getConnectionString());
+        frontendStarter.overrideProperty(Configs.SCHEMA_REPOSITORY_SERVER_URL, schemaRegistry.getUrl());
+        frontendStarter.overrideProperty(Configs.MESSAGES_LOCAL_STORAGE_DIRECTORY, Files.createTempDir().getAbsolutePath());
+        frontendStarter.start();
+
+//        hermesFrontend = HermesFrontend.frontend()
+//                .withBinding(configFactory, ConfigFactory.class)
+//                .withSslContextFactory(customSslContextFactory)
+//                .build();
+//        hermesFrontend.start();
 
         // when
-        SslContextFactoryProvider sslContextFactoryProvider = hermesFrontend.getService(SslContextFactoryProvider.class);
+//        SslContextFactoryProvider sslContextFactoryProvider = hermesFrontend.getService(SslContextFactoryProvider.class);
+        SslContextFactoryProvider sslContextFactoryProvider = frontendStarter.instance().getBean(SslContextFactoryProvider.class);//TODO - IoC
 
         // then
         assertThat(sslContextFactoryProvider.getSslContextFactory()).isEqualTo(customSslContextFactory);
     }
 
     @AfterClass
-    public void tearDown() throws InterruptedException {
-        hermesFrontend.stop();
+    public void tearDown() throws Exception {
+//        hermesFrontend.stop();
+        frontendStarter.stop();
     }
 }
