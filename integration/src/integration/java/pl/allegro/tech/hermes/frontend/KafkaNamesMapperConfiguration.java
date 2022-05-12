653a03288afb13f11fbfcaee60462e98b227101b
Mateusz Szczygiel <mateusz.szczygiel@allegro.pl>
2022-05-12 12:32:49 +0200

    Code review fixes

--- integration/src/integration/java/pl/allegro/tech/hermes/frontend/KafkaNamesMapperConfiguration.java
+++ integration/src/integration/java/pl/allegro/tech/hermes/frontend/KafkaNamesMapperConfiguration.java
@@ -2,6 +2,7 @@ package pl.allegro.tech.hermes.frontend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import pl.allegro.tech.hermes.common.config.ConfigFactory;
 import pl.allegro.tech.hermes.common.config.Configs;
@@ -12,6 +13,7 @@ import pl.allegro.tech.hermes.integration.env.IntegrationTestKafkaNamesMapperFac
 public class KafkaNamesMapperConfiguration {
 
     @Bean
+    @Primary
     @Profile("integration")
     public KafkaNamesMapper testKafkaNamesMapper(ConfigFactory configFactory) {
         return new IntegrationTestKafkaNamesMapperFactory(configFactory.getStringProperty(Configs.KAFKA_NAMESPACE)).create();
