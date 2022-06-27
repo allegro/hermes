90b800af42bd925a8c8f77cf162776cbba761ee6
Mateusz Szczygiel <mateusz.szczygiel@allegro.pl>
2022-06-27 12:05:08 +0200

    Fixed merging bug

--- integration/src/integration/java/pl/allegro/tech/hermes/frontend/FrontendConfigurationProperties.java
+++ integration/src/integration/java/pl/allegro/tech/hermes/frontend/FrontendConfigurationProperties.java
@@ -16,10 +16,6 @@ public class FrontendConfigurationProperties {
     public static String FRONTEND_KEEP_ALIVE_HEADER_TIMEOUT_SECONDS = "frontend.keepAliveHeader.timeoutSeconds";
     public static String SCHEMA_CACHE_ENABLED = "frontend.schema.cache.enabled";
     public static String SCHEMA_REPOSITORY_SERVER_URL = "frontend.schema.repository.serverUrl";
-    public static String FRONTEND_AUTHENTICATION_MODE = "frontend.handlers.authentication.mode";
-    public static String FRONTEND_AUTHENTICATION_ENABLED = "frontend.handlers.authentication.enabled";
-    public static String FRONTEND_KEEP_ALIVE_HEADER_ENABLED = "frontend.handlers.keepAliveHeader.enabled";
-    public static String FRONTEND_KEEP_ALIVE_HEADER_TIMEOUT_SECONDS = "frontend.handlers.keepAliveHeader.timeoutSeconds";
     public static String FRONTEND_SSL_ENABLED = "frontend.ssl.enabled";
     public static String FRONTEND_SSL_PORT = "frontend.ssl.port";
     public static String FRONTEND_SSL_KEYSTORE_SOURCE = "frontend.ssl.keystoreSource";
