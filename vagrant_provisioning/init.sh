f9f23f481f0ac74ba5f4daf3850c72eb59017a6f
Mateusz Szczygiel <mateusz.szczygiel@allegro.pl>
2022-07-19 17:30:14 +0200

    Removed vagrant, rely only on docker

--- vagrant_provisioning/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-add-apt-repository ppa:openjdk-r/ppa 2>/dev/null
-apt-get -y update
-apt-get -qq install curl zip unzip jq git openjdk-8-jdk python-pip
-
-source /vagrant/vagrant_provisioning/install_supervisord.sh
-source /vagrant/vagrant_provisioning/install_kafka.sh
-source /vagrant/vagrant_provisioning/install_puppet_modules.sh
