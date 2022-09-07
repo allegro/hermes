36b4bce7e66558130e0a682fa7a2edf98175cec0
Daniel Fąderski <faderskd@users.noreply.github.com>
2022-09-07 15:52:34 +0200

    Add user info to endpoint ownership validator

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/subscription/validator/EndpointOwnershipValidator.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/subscription/validator/EndpointOwnershipValidator.java
@@ -2,8 +2,9 @@ package pl.allegro.tech.hermes.management.domain.subscription.validator;
 
 import pl.allegro.tech.hermes.api.EndpointAddress;
 import pl.allegro.tech.hermes.api.OwnerId;
+import pl.allegro.tech.hermes.management.domain.auth.RequestUser;
 
 public interface EndpointOwnershipValidator {
 
-    void check(OwnerId owner, EndpointAddress endpoint);
+    void check(OwnerId owner, RequestUser createdBy, EndpointAddress endpoint);
 }
--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/subscription/validator/NoOpEndpointOwnershipValidator.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/subscription/validator/NoOpEndpointOwnershipValidator.java
@@ -2,11 +2,12 @@ package pl.allegro.tech.hermes.management.domain.subscription.validator;
 
 import pl.allegro.tech.hermes.api.EndpointAddress;
 import pl.allegro.tech.hermes.api.OwnerId;
+import pl.allegro.tech.hermes.management.domain.auth.RequestUser;
 
 public class NoOpEndpointOwnershipValidator implements EndpointOwnershipValidator {
 
     @Override
-    public void check(OwnerId owner, EndpointAddress endpoint) {
+    public void check(OwnerId owner, RequestUser createdBy, EndpointAddress endpoint) {
 
     }
 }
