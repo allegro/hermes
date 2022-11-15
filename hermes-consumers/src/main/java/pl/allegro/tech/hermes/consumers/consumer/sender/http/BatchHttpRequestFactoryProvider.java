fc9ec30daebf4a3ad445775180093192840f2918
maciej.moscicki <maciej.moscicki@allegro.pl>
2022-11-15 15:30:56 +0100

    cleanup

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/http/BatchHttpRequestFactoryProvider.java
+++
@@ -1,8 +0,0 @@
-package pl.allegro.tech.hermes.consumers.consumer.sender.http;
-
-import org.eclipse.jetty.client.HttpClient;
-import pl.allegro.tech.hermes.api.Subscription;
-
-public interface BatchHttpRequestFactoryProvider {
-    BatchHttpRequestFactory provideRequestFactory(Subscription subscription, HttpClient httpClient);
-}
