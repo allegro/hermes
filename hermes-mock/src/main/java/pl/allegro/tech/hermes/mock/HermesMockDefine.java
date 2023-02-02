f2a632a3b0c822e0de6a98e0a2921fb8463acc25
Sebastian Straburzynski <sebastian.straburzynski@allegro.pl>
2023-02-02 13:50:38 +0100

    hermes-mock - stub mappings remove support

--- hermes-mock/src/main/java/pl/allegro/tech/hermes/mock/HermesMockDefine.java
+++ hermes-mock/src/main/java/pl/allegro/tech/hermes/mock/HermesMockDefine.java
@@ -2,6 +2,7 @@ package pl.allegro.tech.hermes.mock;
 
 import com.github.tomakehurst.wiremock.http.Request;
 import com.github.tomakehurst.wiremock.matching.ValueMatcher;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import org.apache.avro.Schema;
 import org.apache.http.HttpStatus;
 import pl.allegro.tech.hermes.mock.exchange.Response;
@@ -20,45 +21,49 @@ public class HermesMockDefine {
         this.hermesMockHelper = hermesMockHelper;
     }
 
-    public void jsonTopic(String topicName) {
-        jsonTopic(topicName, HttpStatus.SC_CREATED);
+    public StubMapping jsonTopic(String topicName) {
+        return jsonTopic(topicName, HttpStatus.SC_CREATED);
     }
 
-    public void jsonTopic(String topicName, int statusCode) {
-        addTopic(topicName, aResponse().withStatusCode(statusCode).build(), APPLICATION_JSON);
+    public StubMapping jsonTopic(String topicName, int statusCode) {
+        return addTopic(topicName, aResponse().withStatusCode(statusCode).build(), APPLICATION_JSON);
     }
 
-    public void jsonTopic(String topicName, Response response) {
-        addTopic(topicName, response, APPLICATION_JSON);
+    public StubMapping jsonTopic(String topicName, Response response) {
+        return addTopic(topicName, response, APPLICATION_JSON);
     }
 
-    public <T> void jsonTopic(String topicName, Response response, Class<T> clazz, Predicate<T> predicate) {
+    public <T> StubMapping jsonTopic(String topicName, Response response, Class<T> clazz, Predicate<T> predicate) {
         ValueMatcher<Request> jsonMatchesPattern = ContentMatchers.matchJson(hermesMockHelper, predicate, clazz);
-        addTopic(topicName, response, APPLICATION_JSON, jsonMatchesPattern);
+        return addTopic(topicName, response, APPLICATION_JSON, jsonMatchesPattern);
     }
 
-    public void avroTopic(String topicName) {
-        avroTopic(topicName, HttpStatus.SC_CREATED);
+    public StubMapping avroTopic(String topicName) {
+        return avroTopic(topicName, HttpStatus.SC_CREATED);
     }
 
-    public void avroTopic(String topicName, int statusCode) {
-        addTopic(topicName, aResponse().withStatusCode(statusCode).build(), AVRO_BINARY);
+    public StubMapping avroTopic(String topicName, int statusCode) {
+        return addTopic(topicName, aResponse().withStatusCode(statusCode).build(), AVRO_BINARY);
     }
 
-    public void avroTopic(String topicName, Response response) {
-        addTopic(topicName, response, AVRO_BINARY);
+    public StubMapping avroTopic(String topicName, Response response) {
+        return addTopic(topicName, response, AVRO_BINARY);
     }
 
-    public <T> void avroTopic(String topicName, Response response, Schema schema, Class<T> clazz, Predicate<T> predicate) {
+    public <T> StubMapping avroTopic(String topicName, Response response, Schema schema, Class<T> clazz, Predicate<T> predicate) {
         ValueMatcher<Request> avroMatchesPattern = ContentMatchers.matchAvro(hermesMockHelper, predicate, schema, clazz);
-        addTopic(topicName, response, AVRO_BINARY, avroMatchesPattern);
+        return addTopic(topicName, response, AVRO_BINARY, avroMatchesPattern);
     }
 
-    private void addTopic(String topicName, Response response, String contentType) {
-        hermesMockHelper.addStub(topicName, response, contentType);
+    public void removeStubMapping(StubMapping stubMapping) {
+        hermesMockHelper.removeStubMapping(stubMapping);
     }
 
-    private void addTopic(String topicName, Response response, String contentType, ValueMatcher<Request> valueMatcher) {
-        hermesMockHelper.addStub(topicName, response, contentType, valueMatcher);
+    private StubMapping addTopic(String topicName, Response response, String contentType) {
+        return hermesMockHelper.addStub(topicName, response, contentType);
+    }
+
+    private StubMapping addTopic(String topicName, Response response, String contentType, ValueMatcher<Request> valueMatcher) {
+        return hermesMockHelper.addStub(topicName, response, contentType, valueMatcher);
     }
 }
