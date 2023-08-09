369daa2ea87f60298e08fd5d153efe764a26a597
Daniel Fąderski <daniel.faderski@allegro.pl>
2023-08-09 17:23:32 +0200

    Move topic & subscription metrics names to static classes

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/metric/SubscriptionMetrics.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/metric/SubscriptionMetrics.java
@@ -14,21 +14,7 @@ import pl.allegro.tech.hermes.metrics.counters.HermesCounters;
 import java.util.concurrent.TimeUnit;
 import java.util.function.ToDoubleFunction;
 
-import static pl.allegro.tech.hermes.common.metric.Counters.DELIVERED;
-import static pl.allegro.tech.hermes.common.metric.Counters.DISCARDED;
-import static pl.allegro.tech.hermes.common.metric.Meters.DISCARDED_METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.DISCARDED_SUBSCRIPTION_METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.DISCARDED_TOPIC_METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.FAILED_METER_SUBSCRIPTION;
-import static pl.allegro.tech.hermes.common.metric.Meters.FILTERED_METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.SUBSCRIPTION_BATCH_METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.SUBSCRIPTION_METER;
-import static pl.allegro.tech.hermes.common.metric.Meters.SUBSCRIPTION_THROUGHPUT_BYTES;
-import static pl.allegro.tech.hermes.common.metric.Meters.TOPIC_METER;
 import static pl.allegro.tech.hermes.common.metric.SubscriptionTagsFactory.subscriptionTags;
-import static pl.allegro.tech.hermes.common.metric.Timers.CONSUMER_IDLE_TIME;
-import static pl.allegro.tech.hermes.common.metric.Timers.SUBSCRIPTION_LATENCY;
 
 public class SubscriptionMetrics {
     private final HermesMetrics hermesMetrics;
@@ -48,61 +34,61 @@ public class SubscriptionMetrics {
 
     public HermesCounter successes(SubscriptionName subscription) {
         return size -> {
-            hermesMetrics.meter(METER).mark(size);
-            hermesMetrics.meter(TOPIC_METER, subscription.getTopicName()).mark(size);
-            hermesMetrics.meter(SUBSCRIPTION_METER, subscription.getTopicName(), subscription.getName()).mark(size);
-            hermesMetrics.counter(DELIVERED, subscription.getTopicName(), subscription.getName()).inc(size);
-            micrometerCounter("subscription.delivered", subscription).increment(size);
+            hermesMetrics.meter(Meters.METER).mark(size);
+            hermesMetrics.meter(Meters.TOPIC_METER, subscription.getTopicName()).mark(size);
+            hermesMetrics.meter(Meters.SUBSCRIPTION_METER, subscription.getTopicName(), subscription.getName()).mark(size);
+            hermesMetrics.counter(Counters.DELIVERED, subscription.getTopicName(), subscription.getName()).inc(size);
+            micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_DELIVERED, subscription).increment(size);
         };
     }
 
     public HermesCounter batchSuccesses(SubscriptionName subscription) {
         return HermesCounters.from(
-                micrometerCounter("subscription.batches", subscription),
-                hermesMetrics.meter(SUBSCRIPTION_BATCH_METER, subscription.getTopicName(), subscription.getName())
+                micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_BATCHES, subscription),
+                hermesMetrics.meter(Meters.SUBSCRIPTION_BATCH_METER, subscription.getTopicName(), subscription.getName())
         );
     }
 
     public HermesCounter discarded(SubscriptionName subscription) {
         return size -> {
-            hermesMetrics.meter(DISCARDED_METER).mark(size);
-            hermesMetrics.meter(DISCARDED_TOPIC_METER, subscription.getTopicName()).mark(size);
-            hermesMetrics.meter(DISCARDED_SUBSCRIPTION_METER, subscription.getTopicName(), subscription.getName()).mark(size);
-            hermesMetrics.counter(DISCARDED, subscription.getTopicName(), subscription.getName()).inc(size);
-            micrometerCounter("subscription.discarded", subscription).increment(size);
+            hermesMetrics.meter(Meters.DISCARDED_METER).mark(size);
+            hermesMetrics.meter(Meters.DISCARDED_TOPIC_METER, subscription.getTopicName()).mark(size);
+            hermesMetrics.meter(Meters.DISCARDED_SUBSCRIPTION_METER, subscription.getTopicName(), subscription.getName()).mark(size);
+            hermesMetrics.counter(Counters.DISCARDED, subscription.getTopicName(), subscription.getName()).inc(size);
+            micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_DISCARDED, subscription).increment(size);
         };
     }
 
     public HermesTimer latency(SubscriptionName subscription) {
         return HermesTimer.from(
-                meterRegistry.timer("subscription.latency", subscriptionTags(subscription)),
-                hermesMetrics.timer(SUBSCRIPTION_LATENCY, subscription.getTopicName(), subscription.getName())
+                meterRegistry.timer(SubscriptionMetricsNames.SUBSCRIPTION_LATENCY, subscriptionTags(subscription)),
+                hermesMetrics.timer(Timers.SUBSCRIPTION_LATENCY, subscription.getTopicName(), subscription.getName())
         );
     }
 
     public <T> void registerInflightGauge(SubscriptionName subscription, T obj, ToDoubleFunction<T> f) {
         hermesMetrics.registerInflightGauge(subscription, () -> (int) f.applyAsDouble(obj));
-        meterRegistry.gauge("subscription.inflight", subscriptionTags(subscription), obj, f);
+        meterRegistry.gauge(SubscriptionMetricsNames.SUBSCRIPTION_INFLIGHT, subscriptionTags(subscription), obj, f);
     }
 
     public HermesTimer consumerIdleTimer(SubscriptionName subscription) {
         return HermesTimer.from(
-                meterRegistry.timer("subscription.idle-duration", subscriptionTags(subscription)),
-                hermesMetrics.timer(CONSUMER_IDLE_TIME, subscription.getTopicName(), subscription.getName())
+                meterRegistry.timer(SubscriptionMetricsNames.SUBSCRIPTION_IDLE_DURATION, subscriptionTags(subscription)),
+                hermesMetrics.timer(Timers.CONSUMER_IDLE_TIME, subscription.getTopicName(), subscription.getName())
         );
     }
 
     public HermesCounter filteredOutCounter(SubscriptionName subscription) {
         return HermesCounters.from(
-                micrometerCounter("subscription.filtered-out", subscription),
-                hermesMetrics.meter(FILTERED_METER, subscription.getTopicName(), subscription.getName())
+                micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_FILTERED_OUT, subscription),
+                hermesMetrics.meter(Meters.FILTERED_METER, subscription.getTopicName(), subscription.getName())
         );
     }
 
     public HermesCounter httpAnswerCounter(SubscriptionName subscription, int statusCode) {
         return size -> {
             meterRegistry.counter(
-                    "subscription.http-status-codes",
+                    SubscriptionMetricsNames.SUBSCRIPTION_HTTP_STATUS_CODES,
                     Tags.concat(subscriptionTags(subscription), "status_code", String.valueOf(statusCode))
             ).increment(size);
             hermesMetrics.registerConsumerHttpAnswer(subscription, statusCode, size);
@@ -111,22 +97,22 @@ public class SubscriptionMetrics {
 
     public HermesCounter timeoutsCounter(SubscriptionName subscription) {
         return HermesCounters.from(
-                micrometerCounter("subscription.timeouts", subscription),
+                micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_TIMEOUTS, subscription),
                 hermesMetrics.consumerErrorsTimeoutMeter(subscription)
         );
     }
 
     public HermesCounter otherErrorsCounter(SubscriptionName subscription) {
         return HermesCounters.from(
-                micrometerCounter("subscription.other-errors", subscription),
+                micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_OTHER_ERRORS, subscription),
                 hermesMetrics.consumerErrorsOtherMeter(subscription)
         );
     }
 
     public HermesCounter failuresCounter(SubscriptionName subscription) {
         return HermesCounters.from(
-                micrometerCounter("subscription.failures", subscription),
-                hermesMetrics.meter(FAILED_METER_SUBSCRIPTION, subscription.getTopicName(), subscription.getName())
+                micrometerCounter(SubscriptionMetricsNames.SUBSCRIPTION_FAILURES, subscription),
+                hermesMetrics.meter(Meters.FAILED_METER_SUBSCRIPTION, subscription.getTopicName(), subscription.getName())
         );
     }
 
@@ -143,4 +129,21 @@ public class SubscriptionMetrics {
     private Counter micrometerCounter(String metricName, SubscriptionName subscription) {
         return meterRegistry.counter(metricName, subscriptionTags(subscription));
     }
+
+    public static class SubscriptionMetricsNames {
+        public static final String SUBSCRIPTION_DELIVERED = "subscription.delivered";
+        public static final String SUBSCRIPTION_THROUGHPUT = "subscription.throughput";
+        public static final String SUBSCRIPTION_BATCHES = "subscription.batches";
+        public static final String SUBSCRIPTION_DISCARDED = "subscription.discarded";
+        public static final String SUBSCRIPTION_LATENCY = "subscription.latency";
+        public static final String SUBSCRIPTION_INFLIGHT = "subscription.inflight";
+        public static final String SUBSCRIPTION_IDLE_DURATION = "subscription.idle-duration";
+        public static final String SUBSCRIPTION_FILTERED_OUT = "subscription.filtered-out";
+        public static final String SUBSCRIPTION_HTTP_STATUS_CODES = "subscription.http-status-codes";
+        public static final String SUBSCRIPTION_TIMEOUTS = "subscription.timeouts";
+        public static final String SUBSCRIPTION_OTHER_ERRORS = "subscription.other-errors";
+        public static final String SUBSCRIPTION_FAILURES = "subscription.failures";
+        public static final String SUBSCRIPTION_INFLIGHT_TIME = "subscription.inflight-time";
+    }
+
 }
