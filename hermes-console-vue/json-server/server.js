08214bd1adf7e77517d7422aa1df88c70b3dd5dd
maciej.moscicki <maciej.moscicki@allegro.com>
2023-08-10 17:23:09 +0200

    fix json-server

--- hermes-console-vue/json-server/server.js
+++ hermes-console-vue/json-server/server.js
@@ -1,13 +1,15 @@
-// server.js
 const topics = require('./topics.json');
 const subscriptions = require('./subscriptions.json');
+const routes = require('./routes.json');
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
+
+const rewriter = jsonServer.rewriter(routes);
+server.use(rewriter);
 
 server.post('/query/topics', (req, res) => {
   res.jsonp(topics);
@@ -17,7 +19,9 @@ server.post('/query/subscriptions', (req, res) => {
   res.jsonp(subscriptions);
 });
 
+const router = jsonServer.router('json-server/db.json');
 server.use(router);
+
 server.listen(3000, () => {
   console.log('JSON Server is running');
 });
