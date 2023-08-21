af57a831e803294a6564661f7b7373107d13854f
Mateusz Szczygiel <mateusz.szczygiel@allegro.pl>
2023-08-21 15:25:45 +0200

    Testing

---
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/UiResource.java
@@ -0,0 +1,17 @@
+package pl.allegro.tech.hermes.management.api;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import org.glassfish.jersey.server.mvc.Viewable;
+
+import static jakarta.ws.rs.core.MediaType.TEXT_HTML;
+
+@Path("/")
+public class UiResource {
+    @GET
+    @Produces(TEXT_HTML)
+    public Viewable getIndex() {
+        return new Viewable("/index.html");
+    }
+}
