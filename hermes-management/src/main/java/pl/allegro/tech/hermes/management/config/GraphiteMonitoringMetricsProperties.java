a49f369debc05b5039a15eddb048f1f17a2f7bd2
Daniel Fąderski <daniel.faderski@allegro.pl>
2023-08-29 11:59:45 +0200

    Add code style fixes

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/config/GraphiteMonitoringMetricsProperties.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/config/GraphiteMonitoringMetricsProperties.java
@@ -1,6 +1,6 @@
 package pl.allegro.tech.hermes.management.config;
 
-public class GraphiteMonitoringMetricsProperties extends ExternalMonitoringClientProperties{
+public class GraphiteMonitoringMetricsProperties extends ExternalMonitoringClientProperties {
 
     private String prefix = "stats.tech.hermes";
 
