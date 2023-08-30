b2f5096ce29934ca418325841949e5f7be010b36
Daniel Fąderski <daniel.faderski@allegro.pl>
2023-08-30 18:42:06 +0200

    Test prometheus uri

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/infrastructure/prometheus/RestTemplatePrometheusClient.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/infrastructure/prometheus/RestTemplatePrometheusClient.java
@@ -51,7 +51,6 @@ public class RestTemplatePrometheusClient implements PrometheusClient {
     private PrometheusResponse queryPrometheus(String query) {
         URI queryUri = URI.create(prometheusUri.toString() + "/api/v1/query?query=" + encode(query, UTF_8));
 
-        logger.info("Prom URI: {}", queryUri);
         ResponseEntity<PrometheusResponse> response = restTemplate.exchange(queryUri,
                 HttpMethod.GET, HttpEntity.EMPTY, PrometheusResponse.class);
         return response.getBody();
