f2bd48e0091306934441906e1335a629b0af6c75
Wojciech Sowa <wojciech.a.sowa@allegro.com>
2023-09-07 11:15:10 +0200

    Fix pr

--- hermes-api/src/main/java/pl/allegro/tech/hermes/api/endpoints/MonitoringEndpoint.java
+++ hermes-api/src/main/java/pl/allegro/tech/hermes/api/endpoints/MonitoringEndpoint.java
@@ -3,11 +3,11 @@ package pl.allegro.tech.hermes.api.endpoints;
 import pl.allegro.tech.hermes.api.TopicAndSubscription;
 
 import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("monitoring")
 public interface MonitoringEndpoint {
--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/MonitoringEndpoint.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/MonitoringEndpoint.java
@@ -5,11 +5,11 @@ import pl.allegro.tech.hermes.api.TopicAndSubscription;
 import pl.allegro.tech.hermes.management.infrastructure.monitoring.MonitoringCache;
 
 import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("monitoring")
 public class MonitoringEndpoint {
