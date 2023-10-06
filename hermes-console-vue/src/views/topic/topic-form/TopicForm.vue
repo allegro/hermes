89b48fd19efc2091c83bc8cca5d79aae5d3e5ae5
Sebastian Straburzynski <sebastian.straburzynski@allegro.pl>
2023-10-06 10:18:08 +0200

    Hermes console 2 | ACE editor for debugging subscription filters - TopicForm component cleanup.

--- hermes-console-vue/src/views/topic/topic-form/TopicForm.vue
+++ hermes-console-vue/src/views/topic/topic-form/TopicForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { computed, onMounted, ref } from 'vue';
+  import { computed, ref } from 'vue';
   import { useAppConfigStore } from '@/store/app-config/useAppConfigStore';
   import { useCreateTopic } from '@/composables/topic/use-create-topic/useCreateTopic';
   import { useEditTopic } from '@/composables/topic/use-edit-topic/useEditTopic';
