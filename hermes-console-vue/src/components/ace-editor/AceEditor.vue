ffdd8bf65d952253667fa1e4c27e2c8067f33a5b
Sebastian Straburzynski <sebastian.straburzynski@allegro.pl>
2023-10-06 10:05:41 +0200

    Hermes console 2 | ACE editor for debugging subscription filters - lint fix, ace editor notification title removed.

--- hermes-console-vue/src/components/ace-editor/AceEditor.vue
+++ hermes-console-vue/src/components/ace-editor/AceEditor.vue
@@ -1,10 +1,10 @@
-<script setup lang="ts">
+<script lang="ts" setup>
   import '@/components/ace-editor/ace-config';
   import { defineProps } from 'vue';
-  import { useTheme } from 'vuetify';
-  import { VAceEditor } from 'vue3-ace-editor';
   import { useGlobalI18n } from '@/i18n';
   import { useNotificationsStore } from '@/store/app-notifications/useAppNotifications';
+  import { useTheme } from 'vuetify';
+  import { VAceEditor } from 'vue3-ace-editor';
 
   const theme = useTheme();
   const notificationStore = useNotificationsStore();
@@ -22,7 +22,7 @@
       emit('update:modelValue', JSON.stringify(obj_message, null, 4));
     } catch (e) {
       notificationStore.dispatchNotification({
-        title: t('notifications.form.validationError'),
+        title: '',
         text: t('notifications.form.beautifyError'),
         type: 'error',
       });
@@ -34,16 +34,16 @@
   <div style="border: 1px solid #777777; padding: 10px">
     <p v-if="label" class="v-label">{{ props.label }}</p>
     <v-ace-editor
-      :value="modelValue"
-      @update:value="$emit('update:modelValue', $event)"
-      lang="json"
-      :theme="theme.global.name.value === 'light' ? 'github' : 'monokai'"
-      :placeholder="props.placeholder"
       :options="{ useWorker: true }"
-      style="height: 300px"
+      :placeholder="props.placeholder"
+      :theme="theme.global.name.value === 'light' ? 'github' : 'monokai'"
+      :value="modelValue"
       class="my-3"
+      lang="json"
+      style="height: 300px"
+      @update:value="$emit('update:modelValue', $event)"
     />
-    <v-btn @click="beautify" variant="outlined" color="primary">
+    <v-btn color="primary" variant="outlined" @click="beautify">
       {{ t('topicForm.fields.beautify') }}
     </v-btn>
   </div>
