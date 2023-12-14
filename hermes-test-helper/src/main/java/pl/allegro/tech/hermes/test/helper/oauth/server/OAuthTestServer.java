8cd6c34b1d44f19a4f902ac530f74dfbec7991c0
Piotr Rzysko <piotr.rzysko@gmail.com>
2023-12-14 18:14:44 +0100

    migrate OAuthIntegrationTest to new test framework

--- hermes-test-helper/src/main/java/pl/allegro/tech/hermes/test/helper/oauth/server/OAuthTestServer.java
+++ hermes-test-helper/src/main/java/pl/allegro/tech/hermes/test/helper/oauth/server/OAuthTestServer.java
@@ -1,124 +1,96 @@
 package pl.allegro.tech.hermes.test.helper.oauth.server;
 
-import io.undertow.Handlers;
-import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.handlers.PathHandler;
-import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.api.DeploymentManager;
-import io.undertow.servlet.util.ImmediateInstanceFactory;
-import pl.allegro.tech.hermes.test.helper.util.Ports;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletException;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.apache.http.HttpStatus.SC_OK;
 
 public class OAuthTestServer {
 
-    public static final String OAUTH2_TOKEN_ENDPOINT = "/oauth2/token";
-    public static final String OAUTH2_RESOURCE_ENDPOINT = "/oauth2/resource";
+    private static final String OAUTH2_TOKEN_ENDPOINT = "/oauth2/token";
 
-    private final OAuthTestServerStorage storage = new OAuthTestServerStorage();
-    private final Undertow undertow;
-    private final int port;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final WireMockServer wireMockServer;
 
     public OAuthTestServer() {
-        port = Ports.nextAvailable();
-
-        this.undertow = Undertow.builder()
-            .addHttpListener(port, "localhost")
-            .setHandler(createPathHandler())
-            .build();
+        wireMockServer = new WireMockServer(0);
     }
 
-    private PathHandler createPathHandler() {
+    public String getTokenEndpoint() {
+        return String.format("http://localhost:%s%s", wireMockServer.port(), OAUTH2_TOKEN_ENDPOINT);
+    }
+
+    public String stubAccessTokenForPasswordGrant(OAuthClient client, OAuthResourceOwner resourceOwner) {
+        String token = UUID.randomUUID().toString();
+        Map<String, String> params = Map.of(
+                "access_token", token,
+                "token_type", "Bearer"
+        );
         try {
-            Servlet tokenServlet = new OAuthAccessTokenServlet(storage);
-            Servlet resourceServlet = new OAuthResourceServlet(storage);
-
-            DeploymentInfo deploymentInfo = Servlets.deployment()
-                    .setClassLoader(OAuthTestServer.class.getClassLoader())
-                    .setContextPath("/")
-                    .setDeploymentName("OAuthTestServer")
-                    .addServlets(
-                            Servlets.servlet("AuthTokenServlet", OAuthAccessTokenServlet.class,
-                                    new ImmediateInstanceFactory<>(tokenServlet))
-                                    .addMapping(OAUTH2_TOKEN_ENDPOINT),
-                            Servlets.servlet("ResourceServlet", OAuthResourceServlet.class,
-                                    new ImmediateInstanceFactory<>(resourceServlet))
-                                    .addMapping(OAUTH2_RESOURCE_ENDPOINT)
-                    );
-
-            DeploymentManager deploymentManager = Servlets.defaultContainer().addDeployment(deploymentInfo);
-            deploymentManager.deploy();
-            HttpHandler servletHandler = deploymentManager.start();
-            return Handlers.path().addPrefixPath("/", servletHandler);
-        } catch (ServletException e) {
+            String body = objectMapper.writeValueAsString(params);
+            wireMockServer.addStubMapping(
+                    post(urlPathEqualTo(OAUTH2_TOKEN_ENDPOINT))
+                            .withQueryParam("grant_type", equalTo("password"))
+                            .withQueryParam("scope", equalTo(""))
+                            .withQueryParam("client_id", equalTo(client.clientId()))
+                            .withQueryParam("client_secret", equalTo(client.secret()))
+                            .withQueryParam("username", equalTo(resourceOwner.username()))
+                            .withQueryParam("password", equalTo(resourceOwner.password()))
+                            .willReturn(
+                                    aResponse()
+                                            .withBody(body)
+                                            .withStatus(SC_OK)
+                            )
+                            .build());
+            return token;
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String getTokenEndpoint() {
-        return String.format("http://localhost:%s%s", port, OAUTH2_TOKEN_ENDPOINT);
+    public String stubAccessTokenForClientCredentials(OAuthClient client) {
+        String token = UUID.randomUUID().toString();
+        Map<String, String> params = Map.of(
+                "access_token", token,
+                "token_type", "Bearer"
+        );
+        try {
+            String body = objectMapper.writeValueAsString(params);
+            wireMockServer.addStubMapping(
+                    post(urlPathEqualTo(OAUTH2_TOKEN_ENDPOINT))
+                            .withQueryParam("grant_type", equalTo("client_credentials"))
+                            .withQueryParam("scope", equalTo(""))
+                            .withQueryParam("client_id", equalTo(client.clientId()))
+                            .withQueryParam("client_secret", equalTo(client.secret()))
+                            .willReturn(
+                                    aResponse()
+                                            .withBody(body)
+                                            .withStatus(SC_OK)
+                            )
+                            .build());
+            return token;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public String getUsernamePasswordSecuredResourceEndpoint(String username) {
-        return String.format("http://localhost:%s%s?username=%s", port, OAUTH2_RESOURCE_ENDPOINT, username);
-    }
-
-    public String getClientCredentialsSecuredResourceEndpoint(String clientId) {
-        return String.format("http://localhost:%s%s?clientId=%s", port, OAUTH2_RESOURCE_ENDPOINT, clientId);
-    }
-
-    public void registerClient(String clientId, String clientSecret) {
-        storage.addClient(clientId, clientSecret);
-    }
-
-    public void registerResourceOwner(String username, String password) {
-        storage.addResourceOwner(username, password);
-    }
-
-    public String issueAccessToken(String username) {
-        return storage.issueToken(username);
-    }
-
-    public void clearStorage() {
-        storage.clearAll();
-    }
-
-    public void unregisterAllClients() {
-        storage.clearClients();
-    }
-
-    public void unregisterAllResourceOwners() {
-        storage.clearOwners();
-    }
-
-    public void revokeAllTokens() {
-        storage.clearTokens();
-    }
-
-    public void clearResourceAccessCounters() {
-        storage.clearAccessCounters();
-    }
-
-    public int getResourceAccessCount(String owner) {
-        return storage.getResourceAccessCount(owner);
-    }
-
-    public void clearTokenIssueCounters() {
-        storage.clearTokenIssueCounters();
-    }
-
-    public int getTokenIssueCount(String username) {
-        return storage.getTokenIssueCount(username);
+    public void reset() {
+        wireMockServer.resetAll();
     }
 
     public void start() {
-        undertow.start();
+        wireMockServer.start();
     }
 
     public void stop() {
-        undertow.stop();
+        wireMockServer.stop();
     }
 }
