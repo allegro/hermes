feac3c0887d83e4b4794e65d5af2e9cc89e0d80e
maciej.moscicki <maciej.moscicki@allegro.com>
2024-02-20 13:35:17 +0100

    add multi dc fallback test

--- hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/producer/kafka/KafkaBrokerMessageProducer2.java
+++ hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/producer/kafka/KafkaBrokerMessageProducer2.java
@@ -1,11 +1,11 @@
 package pl.allegro.tech.hermes.frontend.producer.kafka;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import jakarta.inject.Singleton;
-import net.jodah.failsafe.CircuitBreakerOpenException;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.PartitionInfo;
 import org.slf4j.Logger;
@@ -15,11 +15,12 @@ import pl.allegro.tech.hermes.frontend.metric.CachedTopic;
 import pl.allegro.tech.hermes.frontend.producer.BrokerMessageProducer;
 import pl.allegro.tech.hermes.frontend.publishing.PublishingCallback;
 import pl.allegro.tech.hermes.frontend.publishing.message.Message;
+import pl.allegro.tech.hermes.metrics.HermesTimerContext;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class KafkaBrokerMessageProducer2 implements BrokerMessageProducer {
@@ -44,6 +45,15 @@ public class KafkaBrokerMessageProducer2 implements BrokerMessageProducer {
 
         // https://resilience4j.readme.io/docs/circuitbreaker
         // Create a custom configuration for a CircuitBreaker
+        CircuitBreakerConfig exceptionOnlyCircuitBreaker = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000))
+                .permittedNumberOfCallsInHalfOpenState(3)
+                .minimumNumberOfCalls(10)
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
+                .slidingWindowSize(5)
+                .build();
+
         CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
                 .failureRateThreshold(50)
                 .slowCallRateThreshold(50)
@@ -53,27 +63,11 @@ public class KafkaBrokerMessageProducer2 implements BrokerMessageProducer {
                 .minimumNumberOfCalls(10)
                 .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
                 .slidingWindowSize(5)
-                .recordException(e -> INTERNAL_SERVER_ERROR
-                        .equals(getResponse().getStatus()))
-                .recordExceptions(IOException.class, TimeoutException.class)
-                .ignoreExceptions(BusinessException.class, OtherBusinessException.class)
                 .build();
 
 // Create a CircuitBreakerRegistry with a custom global configuration
 
-        circuitBreakerRegistry = CircuitBreakerRegistry.of(circuitBreakerConfig);
-
-//// Get or create a CircuitBreaker from the CircuitBreakerRegistry
-//// with the global default configuration
-//        CircuitBreaker circuitBreakerWithDefaultConfig =
-//                circuitBreakerRegistry.circuitBreaker("name1");
-//
-//// Get or create a CircuitBreaker from the CircuitBreakerRegistry
-//// with a custom configuration
-//        CircuitBreaker circuitBreakerWithCustomConfig = circuitBreakerRegistry
-//                .circuitBreaker("name2", circuitBreakerConfig);
-
-
+        circuitBreakerRegistry = CircuitBreakerRegistry.of(exceptionOnlyCircuitBreaker);
     }
 
     // questions
@@ -83,38 +77,47 @@ public class KafkaBrokerMessageProducer2 implements BrokerMessageProducer {
     // 2. reverse of 1). Slowly move traffic to remote DC.
     // 3. Time budget for local. On timeout, fallback to remote.
     // 4. Multi strategy - on large latency pick more aggressive fallback
-    public Producer<byte[], byte[]> getProducer(CachedTopic cachedTopic) {
+    public Optional<Producer<byte[], byte[]>> getRemoteProducer(CachedTopic cachedTopic) {
+        // 1. local: healthy, enabled    | remote: healthy, enabled <- standardowy przypadek obsłużony przez circuit breaker
+        // 2. local: healthy, disabled   | remote: healthy, enabled <- nie robimy
+        // 3. local: unhealthy, enabled  | remote: healthy, enabled <- to samo co 1
+        // 4. local: unhealthy, disabled | remote: healthy, enabled <- to samo co 2
+        // 5. local: healthy, enabled    | remote: healthy, disable <- done
+        // 6. local: healthy, enabled    | remote: unhealthy, enabled <- niemożliwy przy 100% sukcesów
+        // 7. local: unhealthy, enabled  | remote: unhealthy, enabled <- jeszcze nie zrobione, w tym przypadku wolimy local
 
+        // zwróć Optional.empty() gdy: disabled lub unhealthy
         return producers.get(cachedTopic.getTopic());
     }
 
+
+    // If exception rate greater than X, switch to remote DC for a topic
     @Override
     public void send(Message message, CachedTopic cachedTopic, final PublishingCallback callback) {
         // TODO: report per broker latency metrics
+        var producerRecord = messageConverter.convertToProducerRecord(message, cachedTopic.getKafkaTopics().getPrimary().name());
 
-
-        ProducerRecord<byte[], byte[]> producerRecord =
-                messageConverter.convertToProducerRecord(message, cachedTopic.getKafkaTopics().getPrimary().name());
+        Optional<Producer<byte[], byte[]>> remoteProducer = getRemoteProducer(cachedTopic);
+        var producer = producers.get(cachedTopic.getTopic());
+        HermesTimerContext timer = cachedTopic.startBrokerLatencyTimer();
 
         try {
-//            var producer = getProducer(cachedTopic);
 
-            // circuit breaker to local DC
-            var circuitBreaker = circuitBreakerRegistry.circuitBreaker(cachedTopic.getQualifiedName());
-
-            try {
-                circuitBreaker.decorateRunnable(() -> {
-                    var producer = producers.get(cachedTopic.getTopic());
-
-                    // async
-                    producer.send(producerRecord, new SendCallback(message, cachedTopic, callback));
-                });
-
-            } catch (CircuitBreakerOpenException open) {
-                
+            if (remoteProducer.isEmpty()) {
+                producer.send(producerRecord, new SendCallback(message, cachedTopic, callback, timer));
+            } else {
+                var circuitBreaker = circuitBreakerRegistry.circuitBreaker(cachedTopic.getQualifiedName());
+                try {
+                    circuitBreaker.acquirePermission();
+                    producer.send(producerRecord, new CbSendCallback(message, cachedTopic, callback, circuitBreaker, timer));
+                } catch (CallNotPermittedException open) {
+                    // todo: what if remote DC is disabled or unhealthy?
+                    remoteProducer.get().send(producerRecord, new SendCallback(message, cachedTopic, callback, timer));
+                } catch (Exception e) {
+                    circuitBreaker.releasePermission();
+                    throw e;
+                }
             }
-
-            // todo: fallback to remote
         } catch (Exception e) {
             // message didn't get to internal producer buffer and it will not be send to a broker
             callback.onUnpublished(message, cachedTopic.getTopic(), e);
@@ -161,19 +164,53 @@ public class KafkaBrokerMessageProducer2 implements BrokerMessageProducer {
         private final Message message;
         private final CachedTopic topic;
         private final PublishingCallback callback;
+        private final HermesTimerContext hermesTimerContext;
 
-        public SendCallback(Message message, CachedTopic topic, PublishingCallback callback) {
+        public SendCallback(Message message, CachedTopic topic, PublishingCallback callback, HermesTimerContext hermesTimerContext) {
             this.message = message;
             this.topic = topic;
             this.callback = callback;
+            this.hermesTimerContext = hermesTimerContext;
         }
 
         @Override
         public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            hermesTimerContext.close();
             if (e == null) {
                 callback.onPublished(message, topic.getTopic());
 //                producers.maybeRegisterNodeMetricsGauges(metricsFacade);
             } else {
+                callback.onUnpublished(message, topic.getTopic(), e);
+            }
+        }
+    }
+
+
+    private class CbSendCallback implements org.apache.kafka.clients.producer.Callback {
+
+        private final Message message;
+        private final CachedTopic topic;
+        private final PublishingCallback callback;
+        private final CircuitBreaker circuitBreaker;
+        private final HermesTimerContext hermesTimerContext;
+
+        public CbSendCallback(Message message, CachedTopic topic, PublishingCallback callback, CircuitBreaker circuitBreaker, HermesTimerContext hermesTimerContext) {
+            this.message = message;
+            this.topic = topic;
+            this.callback = callback;
+            this.circuitBreaker = circuitBreaker;
+            this.hermesTimerContext = hermesTimerContext;
+        }
+
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            var duration = hermesTimerContext.closeAndGet();
+            if (e == null) {
+                circuitBreaker.onSuccess(duration.toMillis(), TimeUnit.MILLISECONDS);
+                callback.onPublished(message, topic.getTopic());
+//                producers.maybeRegisterNodeMetricsGauges(metricsFacade);
+            } else {
+                circuitBreaker.onError(duration.toMillis(), TimeUnit.MILLISECONDS, e);
                 callback.onUnpublished(message, topic.getTopic(), e);
             }
         }
