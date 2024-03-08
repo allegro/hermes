0040e006ebfeb8c0ee9f1c46070865c45b3218d7
Wojciech Sowa <wojciech.a.sowa@allegro.com>
2024-03-08 13:42:44 +0100

    Checkstyle

--- hermes-api/src/main/java/pl/allegro/tech/hermes/api/endpoints/MonitoringEndpoint.java
+++ hermes-api/src/main/java/pl/allegro/tech/hermes/api/endpoints/MonitoringEndpoint.java
@@ -1,11 +1,11 @@
 package pl.allegro.tech.hermes.api.endpoints;
 
-import pl.allegro.tech.hermes.api.TopicAndSubscription;
-
-import java.util.List;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import pl.allegro.tech.hermes.api.TopicAndSubscription;
+
+import java.util.List;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
