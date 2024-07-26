c1f4c73e0ce04f96a52252e649ec9a0f9f846009
maciej.moscicki <maciej.moscicki@allegro.com>
2024-07-26 13:03:39 +0200

    log error when removing ZK topics

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/topic/commands/RemoveTopicRepositoryCommand.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/topic/commands/RemoveTopicRepositoryCommand.java
@@ -3,6 +3,7 @@ package pl.allegro.tech.hermes.management.domain.topic.commands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.allegro.tech.hermes.api.TopicName;
+import pl.allegro.tech.hermes.common.exception.InternalProcessingException;
 import pl.allegro.tech.hermes.domain.topic.TopicRepository;
 import pl.allegro.tech.hermes.management.domain.dc.DatacenterBoundRepositoryHolder;
 import pl.allegro.tech.hermes.management.domain.dc.RepositoryCommand;
@@ -23,7 +24,12 @@ public class RemoveTopicRepositoryCommand extends RepositoryCommand<TopicReposit
     public void execute(DatacenterBoundRepositoryHolder<TopicRepository> holder) {
         logger.info("Removing topic: {} in ZK dc: {}", topicName, holder.getDatacenterName());
         long start = System.currentTimeMillis();
-        holder.getRepository().removeTopic(topicName);
+        try {
+            holder.getRepository().removeTopic(topicName);
+        } catch (InternalProcessingException e) {
+            logger.error("Error while trying to remove topic {} in ZK dc: {}", topicName, holder.getDatacenterName(), e);
+            throw e;
+        }
         logger.info("Removed topic: {} in ZK dc: {}, in {} ms", topicName, holder.getDatacenterName(), System.currentTimeMillis() - start);
 
     }
