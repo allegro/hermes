8057c701e56e392385fbc06df719f60310d0bbe8
Maciej Moscicki <maciej.moscicki@allegro.com>
2024-08-21 13:10:32 +0200

    Synchronize zookeeper clusters with UI (#1888)

--- hermes-console/src/views/admin/consistency/inconsistent-metadata/InconsistentMetadata.spec.ts
+++ hermes-console/src/views/admin/consistency/inconsistent-metadata/InconsistentMetadata.spec.ts
@@ -1,9 +1,16 @@
-import { describe, expect } from 'vitest';
+import { beforeEach, describe, expect } from 'vitest';
+import { createPinia, setActivePinia } from 'pinia';
 import { inconsistentMetadata } from '@/dummy/inconsistentMetadata';
-import { render } from '@/utils/test-utils';
+import { render, renderWithEmits } from '@/utils/test-utils';
+import { waitFor } from '@testing-library/vue';
 import InconsistentMetadata from '@/views/admin/consistency/inconsistent-metadata/InconsistentMetadata.vue';
+import router from '@/router';
 
 describe('InconsistentMetadataView', () => {
+  beforeEach(async () => {
+    setActivePinia(createPinia());
+  });
+
   it('should render metadata inconsistencies', () => {
     // when
     const { getByText } = render(InconsistentMetadata, {
@@ -30,5 +37,25 @@ describe('InconsistentMetadataView', () => {
     expect(
       getByText('consistency.inconsistentGroup.metadata.consistent'),
     ).toBeVisible();
+  });
+
+  it('should emit event with selected datacenter when sync button is clicked', async () => {
+    const group = 'pl.allegro.public.group';
+    await router.push(`/ui/consistency/${group}`);
+
+    const wrapper = renderWithEmits(InconsistentMetadata, {
+      props: {
+        metadata: inconsistentMetadata,
+      },
+    });
+
+    const buttonId = `sync-datacenter-${inconsistentMetadata[0].datacenter}`;
+    await wrapper.find(`[data-testid="${buttonId}"]`).trigger('click');
+
+    await waitFor(() => {
+      const syncEvents = wrapper.emitted('sync')!!;
+      expect(syncEvents.length).toEqual(1);
+      expect(syncEvents[0]).toEqual([inconsistentMetadata[0].datacenter]);
+    });
   });
 });
