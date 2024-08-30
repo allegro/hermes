45fa371d12fe0ebcbab56ed0fcbd171dbb654639
maciej.moscicki <maciej.moscicki@allegro.com>
2024-08-30 16:16:34 +0200

    test google java format

--- .github/scripts/check-google-java-format.sh
+++ .github/scripts/check-google-java-format.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
+set -eu
+
+script_name=""
+
+case "$(uname -sr)" in
+
+   Darwin*)
+     script_name="google-java-format_darwin-arm64"
+     ;;
+
+   Linux*)
+     script_name="google-java-format_linux-x86-64"
+     ;;
+   *)
+     echo 'Unsupported OS'
+     exit 1
+     ;;
+esac
 
 JAVA_FILES=$(find . -name "*.java" -type f)
 
 invalid_files=0
 
 echo "Following files are formatted incorrectly:";
+# TODO: remove '--skip-reflowing-long-strings' once https://github.com/google/google-java-format/issues/566 is fixed
 for FILE in $JAVA_FILES; do
-  java -jar "google-java-format-$1-all-deps.jar" --set-exit-if-changed "$FILE" > /dev/null
+  ./$script_name -r --skip-reflowing-long-strings "$FILE" > /dev/null
   if [ $? -ne 0 ]; then
     echo "$FILE"
     ((invalid_files++))
