508d84f9f47d9ce2c0c0ac664bd529fd31bf8ed9
szczygiel-m <szczygiel-m@users.noreply.github.com>
2024-11-07 11:50:34 +0000

    deploy: 6afc3ac6e217c56b4a734abcb9361759e6215b92

--- hermes-tracker-elasticsearch/src/main/java/pl/allegro/tech/hermes/tracker/elasticsearch/consumers/ConsumersIndexFactory.java
+++
@@ -1,6 +0,0 @@
-package pl.allegro.tech.hermes.tracker.elasticsearch.consumers;
-
-import pl.allegro.tech.hermes.tracker.elasticsearch.IndexFactory;
-
-public interface ConsumersIndexFactory extends IndexFactory {
-}
--- hermes-tracker-elasticsearch/src/main/java/pl/allegro/tech/hermes/tracker/elasticsearch/frontend/FrontendIndexFactory.java
+++
@@ -1,6 +0,0 @@
-package pl.allegro.tech.hermes.tracker.elasticsearch.frontend;
-
-import pl.allegro.tech.hermes.tracker.elasticsearch.IndexFactory;
-
-public interface FrontendIndexFactory extends IndexFactory {
-}
