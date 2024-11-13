caf1ca58ca3a3bf6180d7194201aed7bef4b430f
Michal Wisniewski <michal.wisniewski@allegro.com>
2024-11-13 17:33:21 +0100

    SKYEDEN-3234 | Remove unnecessary annotation

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/HermesManagement.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/HermesManagement.java
@@ -2,10 +2,8 @@ package pl.allegro.tech.hermes.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class HermesManagement {
 
   public static void main(String[] args) {
