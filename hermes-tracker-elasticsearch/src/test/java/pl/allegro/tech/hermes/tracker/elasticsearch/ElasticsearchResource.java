f4099e4f31a0a09820754ea86feac349ae3d429e
Mateusz <76775507+szczygiel-m@users.noreply.github.com>
2024-11-21 09:43:51 +0100

    Java 21 (#1928)
    * Introducing Java 21
    
    * Gradle 8.11, fixed CI
    
    * Gradle 8.10

--- hermes-tracker-elasticsearch/src/test/java/pl/allegro/tech/hermes/tracker/elasticsearch/ElasticsearchResource.java
+++ hermes-tracker-elasticsearch/src/test/java/pl/allegro/tech/hermes/tracker/elasticsearch/ElasticsearchResource.java
@@ -1,50 +1,34 @@
 package pl.allegro.tech.hermes.tracker.elasticsearch;
 
-import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.CLUSTER_NAME;
-import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.HTTP_PORT;
-import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.TRANSPORT_TCP_PORT;
-
 import java.net.InetAddress;
-import java.nio.file.Files;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 import org.elasticsearch.client.AdminClient;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.rules.ExternalResource;
-import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
-import pl.allegro.tech.hermes.test.helper.util.Ports;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
 public class ElasticsearchResource extends ExternalResource implements LogSchemaAware {
 
-  private static final String ELASTIC_VERSION = "6.1.4";
+  private static final String ELASTIC_VERSION =
+      "docker.elastic.co/elasticsearch/elasticsearch:7.10.2";
   private static final String CLUSTER_NAME_VALUE = "myTestCluster";
 
-  private final EmbeddedElastic embeddedElastic;
+  private final ElasticsearchContainer elasticsearchContainer;
   private Client client;
 
   public ElasticsearchResource() {
-    int port = Ports.nextAvailable();
-    int httpPort = Ports.nextAvailable();
-
     try {
-      embeddedElastic =
-          EmbeddedElastic.builder()
-              .withElasticVersion(ELASTIC_VERSION)
-              .withSetting(TRANSPORT_TCP_PORT, port)
-              .withSetting(HTTP_PORT, httpPort)
-              .withSetting(CLUSTER_NAME, CLUSTER_NAME_VALUE)
-              // embedded elastic search runs with "UseConcMarkSweepGC" which is invalid in Java 17
-              .withEsJavaOpts("-Xms128m -Xmx512m -XX:+IgnoreUnrecognizedVMOptions")
-              .withStartTimeout(1, TimeUnit.MINUTES)
-              .withCleanInstallationDirectoryOnStop(true)
-              .withInstallationDirectory(
-                  Files.createTempDirectory("elasticsearch-installation-" + port).toFile())
-              .build();
-
+      elasticsearchContainer =
+          new ElasticsearchContainer(ELASTIC_VERSION)
+              .withEnv("cluster.name", CLUSTER_NAME_VALUE)
+              .withEnv("discovery.type", "single-node")
+              .withEnv("ES_JAVA_OPTS", "-Xms128m -Xmx512m -XX:+IgnoreUnrecognizedVMOptions")
+              .withStartupTimeout(Duration.ofMinutes(1));
     } catch (Exception e) {
       throw new RuntimeException("Unchecked exception", e);
     }
@@ -52,19 +36,20 @@ public class ElasticsearchResource extends ExternalResource implements LogSchema
 
   @Override
   public void before() throws Throwable {
-    embeddedElastic.start();
+    elasticsearchContainer.start();
 
     client =
         new PreBuiltTransportClient(
-                Settings.builder().put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build())
+                Settings.builder().put("cluster.name", CLUSTER_NAME_VALUE).build())
             .addTransportAddress(
                 new TransportAddress(
-                    InetAddress.getByName("localhost"), embeddedElastic.getTransportTcpPort()));
+                    InetAddress.getByName(elasticsearchContainer.getHost()),
+                    elasticsearchContainer.getMappedPort(9300)));
   }
 
   @Override
   public void after() {
-    embeddedElastic.stop();
+    elasticsearchContainer.stop();
     client.close();
   }
 
@@ -76,7 +61,7 @@ public class ElasticsearchResource extends ExternalResource implements LogSchema
     return client.admin();
   }
 
-  public ImmutableOpenMap<String, IndexMetaData> getIndices() {
+  public ImmutableOpenMap<String, IndexMetadata> getIndices() {
     return client
         .admin()
         .cluster()
@@ -84,12 +69,12 @@ public class ElasticsearchResource extends ExternalResource implements LogSchema
         .execute()
         .actionGet()
         .getState()
-        .getMetaData()
+        .getMetadata()
         .getIndices();
   }
 
   public void cleanStructures() {
-    embeddedElastic.deleteIndices();
-    embeddedElastic.deleteTemplates();
+    client.admin().indices().prepareDelete("*").get();
+    client.admin().indices().prepareDeleteTemplate("*").get();
   }
 }
