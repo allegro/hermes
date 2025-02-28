553a2cf3ad48da2ba7d76d91fa256427cacba61f
Mateusz <mateusz.szczygiel@allegro.com>
2025-02-28 12:08:53 +0100

    Logging remote datacenters

--- hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/config/KafkaClustersProperties.java
+++ hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/config/KafkaClustersProperties.java
@@ -3,12 +3,16 @@ package pl.allegro.tech.hermes.frontend.config;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import pl.allegro.tech.hermes.common.kafka.KafkaParameters;
 import pl.allegro.tech.hermes.infrastructure.dc.DatacenterNameProvider;
 
 @ConfigurationProperties(prefix = "frontend.kafka")
 public class KafkaClustersProperties {
+
+  Logger logger = LoggerFactory.getLogger(KafkaClustersProperties.class);
 
   private List<KafkaProperties> clusters = new ArrayList<>();
 
@@ -66,11 +70,19 @@ public class KafkaClustersProperties {
                         "No properties for datacenter: " + currentDatacenterName + " defined."))
             .getRemoteDatacenters();
 
-    return this.clusters.stream()
-        .filter(
-            cluster ->
-                remoteDatacenters.contains(cluster.getDatacenter())
-                    && !cluster.getDatacenter().equals(currentDatacenterName))
-        .collect(Collectors.toList());
+    List<KafkaParameters> filteredClusters =
+        this.clusters.stream()
+            .filter(
+                cluster ->
+                    remoteDatacenters.contains(cluster.getDatacenter())
+                        && !cluster.getDatacenter().equals(currentDatacenterName))
+            .collect(Collectors.toList());
+
+    List<String> filteredRemoteDatacenters =
+        filteredClusters.stream().map(KafkaParameters::getDatacenter).collect(Collectors.toList());
+
+    logger.info("Remote datacenters for {}: {}", currentDatacenterName, filteredRemoteDatacenters);
+
+    return filteredClusters;
   }
 }
