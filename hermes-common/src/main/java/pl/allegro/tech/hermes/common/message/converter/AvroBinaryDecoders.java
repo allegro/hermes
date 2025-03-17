b9e783638fe4e47926a25d1f6232d291309d031a
mateusz.dobrowolski <mateusz.dobrowolski@allegro.com>
2025-03-17 14:12:53 +0100

    GH-1808 | Reformat code

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/message/converter/AvroBinaryDecoders.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/message/converter/AvroBinaryDecoders.java
@@ -1,5 +1,10 @@
 package pl.allegro.tech.hermes.common.message.converter;
 
+import java.io.ByteArrayInputStream;
+import java.io.Closeable;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
 import org.apache.avro.Conversion;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
@@ -10,53 +15,49 @@ import org.apache.avro.io.DecoderFactory;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import tech.allegro.schema.json2avro.converter.AvroConversionException;
 
-import java.io.ByteArrayInputStream;
-import java.io.Closeable;
-import java.io.InputStream;
-import java.util.Collection;
-import java.util.Collections;
-
 public class AvroBinaryDecoders {
 
-    private static ThreadLocal<InputStream> threadLocalEmptyInputStream =
-            ThreadLocal.withInitial(() -> new ByteArrayInputStream(new byte[0]));
+  private static ThreadLocal<InputStream> threadLocalEmptyInputStream =
+      ThreadLocal.withInitial(() -> new ByteArrayInputStream(new byte[0]));
 
-    private static ThreadLocal<BinaryDecoder> threadLocalBinaryDecoder =
-            ThreadLocal.withInitial(
-                    () -> DecoderFactory.get().binaryDecoder(threadLocalEmptyInputStream.get(), null));
+  private static ThreadLocal<BinaryDecoder> threadLocalBinaryDecoder =
+      ThreadLocal.withInitial(
+          () -> DecoderFactory.get().binaryDecoder(threadLocalEmptyInputStream.get(), null));
 
-    static GenericRecord decodeReusingThreadLocalBinaryDecoder(byte[] message, Schema schema) {
-        return decodeReusingThreadLocalBinaryDecoder(message, schema, Collections.emptyList());
+  static GenericRecord decodeReusingThreadLocalBinaryDecoder(byte[] message, Schema schema) {
+    return decodeReusingThreadLocalBinaryDecoder(message, schema, Collections.emptyList());
+  }
+
+  static GenericRecord decodeReusingThreadLocalBinaryDecoder(
+      byte[] message, Schema schema, Collection<Conversion<?>> conversions) {
+    try (FlushableBinaryDecoderHolder holder = new FlushableBinaryDecoderHolder()) {
+      BinaryDecoder binaryDecoder =
+          DecoderFactory.get().binaryDecoder(message, holder.getBinaryDecoder());
+      GenericData genericData = new GenericData();
+      conversions.forEach(genericData::addLogicalTypeConversion);
+      return new GenericDatumReader<GenericRecord>(schema, schema, genericData)
+          .read(null, binaryDecoder);
+    } catch (Exception e) {
+      String reason =
+          e.getMessage() == null ? ExceptionUtils.getRootCauseMessage(e) : e.getMessage();
+      throw new AvroConversionException(
+          String.format(
+              "Could not deserialize Avro message with provided schema, reason: %s", reason));
+    }
+  }
+
+  static class FlushableBinaryDecoderHolder implements Closeable {
+
+    final BinaryDecoder binaryDecoder = threadLocalBinaryDecoder.get();
+
+    BinaryDecoder getBinaryDecoder() {
+      return binaryDecoder;
     }
 
-    static GenericRecord decodeReusingThreadLocalBinaryDecoder(byte[] message, Schema schema, Collection<Conversion<?>> conversions) {
-        try (FlushableBinaryDecoderHolder holder = new FlushableBinaryDecoderHolder()) {
-            BinaryDecoder binaryDecoder =
-                    DecoderFactory.get().binaryDecoder(message, holder.getBinaryDecoder());
-            GenericData genericData = new GenericData();
-            conversions.forEach(genericData::addLogicalTypeConversion);
-            return new GenericDatumReader<GenericRecord>(schema, schema, genericData).read(null, binaryDecoder);
-        } catch (Exception e) {
-            String reason =
-                    e.getMessage() == null ? ExceptionUtils.getRootCauseMessage(e) : e.getMessage();
-            throw new AvroConversionException(
-                    String.format(
-                            "Could not deserialize Avro message with provided schema, reason: %s", reason));
-        }
+    @Override
+    public void close() {
+      DecoderFactory.get()
+          .binaryDecoder(threadLocalEmptyInputStream.get(), threadLocalBinaryDecoder.get());
     }
-
-    static class FlushableBinaryDecoderHolder implements Closeable {
-
-        final BinaryDecoder binaryDecoder = threadLocalBinaryDecoder.get();
-
-        BinaryDecoder getBinaryDecoder() {
-            return binaryDecoder;
-        }
-
-        @Override
-        public void close() {
-            DecoderFactory.get()
-                    .binaryDecoder(threadLocalEmptyInputStream.get(), threadLocalBinaryDecoder.get());
-        }
-    }
+  }
 }
--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/converter/AvroToJsonMessageConverter.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/converter/AvroToJsonMessageConverter.java
@@ -1,5 +1,11 @@
 package pl.allegro.tech.hermes.consumers.consumer.converter;
 
+import static java.util.stream.Collectors.toList;
+import static pl.allegro.tech.hermes.common.message.converter.AvroRecordToBytesConverter.bytesToRecord;
+import static pl.allegro.tech.hermes.common.message.wrapper.AvroMetadataMarker.METADATA_MARKER;
+import static pl.allegro.tech.hermes.consumers.consumer.Message.message;
+
+import java.util.List;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
@@ -9,51 +15,45 @@ import pl.allegro.tech.hermes.consumers.consumer.Message;
 import tech.allegro.schema.json2avro.converter.AvroJsonConverter;
 import tech.allegro.schema.json2avro.converter.conversions.DecimalAsStringConversion;
 
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
-import static pl.allegro.tech.hermes.common.message.converter.AvroRecordToBytesConverter.bytesToRecord;
-import static pl.allegro.tech.hermes.common.message.wrapper.AvroMetadataMarker.METADATA_MARKER;
-import static pl.allegro.tech.hermes.consumers.consumer.Message.message;
-
 public class AvroToJsonMessageConverter implements MessageConverter {
 
-    private final AvroJsonConverter converter;
+  private final AvroJsonConverter converter;
 
-    public AvroToJsonMessageConverter() {
-        this.converter = new AvroJsonConverter(DecimalAsStringConversion.INSTANCE);
-    }
+  public AvroToJsonMessageConverter() {
+    this.converter = new AvroJsonConverter(DecimalAsStringConversion.INSTANCE);
+  }
 
-    @Override
-    public Message convert(Message message, Topic topic) {
-        return message()
-                .fromMessage(message)
-                .withContentType(ContentType.JSON)
-                .withData(
-                        converter.convertToJson(
-                                recordWithoutMetadata(
-                                        message.getData(), message.<Schema>getSchema().get().getSchema())))
-                .withNoSchema()
-                .build();
-    }
+  @Override
+  public Message convert(Message message, Topic topic) {
+    return message()
+        .fromMessage(message)
+        .withContentType(ContentType.JSON)
+        .withData(
+            converter.convertToJson(
+                recordWithoutMetadata(
+                    message.getData(), message.<Schema>getSchema().get().getSchema())))
+        .withNoSchema()
+        .build();
+  }
 
-    private GenericRecord recordWithoutMetadata(byte[] data, Schema schema) {
-        GenericRecord original = bytesToRecord(data, schema, List.of(DecimalAsStringConversion.INSTANCE));
-        Schema schemaWithoutMetadata = removeMetadataField(schema);
-        GenericRecordBuilder builder = new GenericRecordBuilder(schemaWithoutMetadata);
-        schemaWithoutMetadata
-                .getFields()
-                .forEach(field -> builder.set(field, original.get(field.name())));
-        return builder.build();
-    }
+  private GenericRecord recordWithoutMetadata(byte[] data, Schema schema) {
+    GenericRecord original =
+        bytesToRecord(data, schema, List.of(DecimalAsStringConversion.INSTANCE));
+    Schema schemaWithoutMetadata = removeMetadataField(schema);
+    GenericRecordBuilder builder = new GenericRecordBuilder(schemaWithoutMetadata);
+    schemaWithoutMetadata
+        .getFields()
+        .forEach(field -> builder.set(field, original.get(field.name())));
+    return builder.build();
+  }
 
-    private Schema removeMetadataField(Schema schema) {
-        return Schema.createRecord(
-                schema.getFields().stream()
-                        .filter(field -> !METADATA_MARKER.equals(field.name()))
-                        .map(
-                                field ->
-                                        new Schema.Field(field.name(), field.schema(), field.doc(), field.defaultVal()))
-                        .collect(toList()));
-    }
+  private Schema removeMetadataField(Schema schema) {
+    return Schema.createRecord(
+        schema.getFields().stream()
+            .filter(field -> !METADATA_MARKER.equals(field.name()))
+            .map(
+                field ->
+                    new Schema.Field(field.name(), field.schema(), field.doc(), field.defaultVal()))
+            .collect(toList()));
+  }
 }
