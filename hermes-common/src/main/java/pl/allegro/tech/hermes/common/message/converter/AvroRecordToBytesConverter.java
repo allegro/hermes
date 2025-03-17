55d162216f8d766edf959ad3b00ed625e8267f76
mateusz.dobrowolski <mateusz.dobrowolski@allegro.com>
2025-03-17 14:01:15 +0100

    GH-1808 | Logical type decimal to json as string

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/message/converter/AvroRecordToBytesConverter.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/message/converter/AvroRecordToBytesConverter.java
@@ -1,25 +1,32 @@
 package pl.allegro.tech.hermes.common.message.converter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import org.apache.avro.Conversion;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.EncoderFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collection;
+
 public interface AvroRecordToBytesConverter {
 
-  static GenericRecord bytesToRecord(byte[] data, Schema schema) {
-    return AvroBinaryDecoders.decodeReusingThreadLocalBinaryDecoder(data, schema);
-  }
+    static GenericRecord bytesToRecord(byte[] data, Schema schema) {
+        return AvroBinaryDecoders.decodeReusingThreadLocalBinaryDecoder(data, schema);
+    }
 
-  static byte[] recordToBytes(GenericRecord genericRecord, Schema schema) throws IOException {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    BinaryEncoder binaryEncoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-    new GenericDatumWriter<>(schema).write(genericRecord, binaryEncoder);
-    binaryEncoder.flush();
+    static GenericRecord bytesToRecord(byte[] data, Schema schema, Collection<Conversion<?>> conversions) {
+        return AvroBinaryDecoders.decodeReusingThreadLocalBinaryDecoder(data, schema, conversions);
+    }
 
-    return outputStream.toByteArray();
-  }
+    static byte[] recordToBytes(GenericRecord genericRecord, Schema schema) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BinaryEncoder binaryEncoder = EncoderFactory.get().binaryEncoder(outputStream, null);
+        new GenericDatumWriter<>(schema).write(genericRecord, binaryEncoder);
+        binaryEncoder.flush();
+
+        return outputStream.toByteArray();
+    }
 }
