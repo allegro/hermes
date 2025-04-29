87216915b20d9b2a451825a165105e205754a012
Daniel Fąderski <daniel.faderski@allegro.pl>
2025-04-29 14:20:43 +0200

    Fix typo in subscription route and change interface visibility to public

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/retransmit/RetransmissionMonitoringUrlProvider.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/domain/retransmit/RetransmissionMonitoringUrlProvider.java
@@ -2,7 +2,7 @@ package pl.allegro.tech.hermes.management.domain.retransmit;
 
 import pl.allegro.tech.hermes.api.OfflineRetransmissionTask;
 
-interface RetransmissionMonitoringUrlProvider {
+public interface RetransmissionMonitoringUrlProvider {
   String getLogsUrl(OfflineRetransmissionTask task);
 
   String getMetricsUrl(OfflineRetransmissionTask task);
