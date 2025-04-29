8a537ba9a7644d9f6088ca752e9e82f3e02a79af
Daniel Fąderski <daniel.faderski@allegro.pl>
2025-04-29 12:32:03 +0200

    Refactor properties for UI for offline retransmission

--- hermes-console/src/api/app-configuration.ts
+++ hermes-console/src/api/app-configuration.ts
@@ -88,11 +88,7 @@ export interface TopicViewConfiguration {
   readOnlyModeEnabled: boolean;
   allowedTopicLabels: string[];
   retentionUnits: RetentionUnit[];
-  offlineRetransmissionEnabled: boolean;
-  offlineRetransmissionDescription: string;
-  offlineRetransmissionFromViewDocsUrl: string;
-  offlineRetransmissionGlobalTaskQueueUrl: string;
-  offlineRetransmissionMonitoringDocsUrl: string;
+  offlineRetransmission: OfflineRetransmission;
 }
 
 export interface DefaultTopicViewConfiguration {
@@ -171,4 +167,12 @@ export interface CostsConfiguration {
   topicDetailsUrl: string;
   subscriptionIframeUrl: string;
   subscriptionDetailsUrl: string;
+}
+
+export interface OfflineRetransmission {
+  enabled: boolean;
+  description: string;
+  fromViewDocsUrl: string;
+  globalTaskQueueUrl: string;
+  monitoringDocsUrl: string;
 }
