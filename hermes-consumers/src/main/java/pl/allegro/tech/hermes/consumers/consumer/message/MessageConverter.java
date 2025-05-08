e198d58fe3782883e0e60ecb3f7330a454949894
Adam <adam.szorcz@allegro.pl>
2025-05-08 22:03:33 +0200

    TANGO-2508 : Deadletter creators - fools proof

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/message/MessageConverter.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/message/MessageConverter.java
@@ -2,6 +2,7 @@ package pl.allegro.tech.hermes.consumers.consumer.message;
 
 import pl.allegro.tech.hermes.api.Subscription;
 import pl.allegro.tech.hermes.consumers.consumer.Message;
+import pl.allegro.tech.hermes.consumers.consumer.sender.MessageSendingResult;
 import pl.allegro.tech.hermes.tracker.consumers.MessageMetadata;
 import pl.allegro.tech.hermes.tracker.consumers.deadletters.DeadMessage;
 
@@ -35,36 +36,43 @@ public class MessageConverter {
         message.getReadingTimestamp());
   }
 
-  public static DeadMessage toDeadMessage(Message message, Subscription subscription, String rootCause) {
-    return new DeadMessage(
-            message.getId(),
-            message.getOffset(),
-            message.getPartition(),
-            message.getPartitionAssignmentTerm(),
-            message.getTopic(),
-            subscription.getName(),
-            message.getKafkaTopic().asString(),
-            message.getPublishingTimestamp(),
-            message.getReadingTimestamp(),
-            message.getData(),
-            rootCause
-            );
+  public static DeadMessage toDeadMessage(Message message, Subscription subscription, MessageSendingResult messageSendingResult) {
+     if (messageSendingResult.getRootCause() != null) {
+       return new DeadMessage(
+               message.getId(),
+               message.getOffset(),
+               message.getPartition(),
+               message.getPartitionAssignmentTerm(),
+               message.getTopic(),
+               subscription.getName(),
+               message.getKafkaTopic().asString(),
+               message.getPublishingTimestamp(),
+               message.getReadingTimestamp(),
+               message.getData(),
+               messageSendingResult.getRootCause()
+       );
+     } else {
+       StringBuilder cause= new StringBuilder(String.format("Status code : %s", messageSendingResult.getStatusCode()));
+       if (messageSendingResult.getLogInfo() != null) {
+          messageSendingResult.getLogInfo().stream()
+                  .filter(logInfo -> logInfo.getRootCause() != null)
+                  .forEach( logInfo -> cause.append(String.format("\n%s, %s", logInfo.getRootCause(), logInfo))
+          );
+       }
+       return new DeadMessage(
+               message.getId(),
+               message.getOffset(),
+               message.getPartition(),
+               message.getPartitionAssignmentTerm(),
+               message.getTopic(),
+               subscription.getName(),
+               message.getKafkaTopic().asString(),
+               message.getPublishingTimestamp(),
+               message.getReadingTimestamp(),
+               message.getData(),
+               cause.toString()
+       );
+     }
   }
 
-  public static DeadMessage toDeadMessage(
-          Message message, Subscription subscription, String rootCause, String batchId) {
-    return new DeadMessage(
-            message.getId(),
-            batchId,
-            message.getOffset(),
-            message.getPartition(),
-            message.getPartitionAssignmentTerm(),
-            subscription.getQualifiedTopicName(),
-            subscription.getName(),
-            message.getKafkaTopic().asString(),
-            message.getPublishingTimestamp(),
-            message.getReadingTimestamp(),
-            message.getData(),
-            rootCause);
-  }
 }
