c64ea6442623a6ee55c09f1b67a18bf6b3fba5e4
Adam <adam.szorcz@allegro.pl>
2025-05-09 09:50:00 +0200

    TANGO-2508 : Custom message for Failed Append Exception

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/message/MessageConverter.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/message/MessageConverter.java
@@ -3,6 +3,7 @@ package pl.allegro.tech.hermes.consumers.consumer.message;
 import pl.allegro.tech.hermes.api.Subscription;
 import pl.allegro.tech.hermes.consumers.consumer.Message;
 import pl.allegro.tech.hermes.consumers.consumer.sender.MessageSendingResult;
+import pl.allegro.tech.hermes.consumers.consumer.sender.SingleMessageSendingResult;
 import pl.allegro.tech.hermes.tracker.consumers.MessageMetadata;
 import pl.allegro.tech.hermes.tracker.consumers.deadletters.DeadMessage;
 
@@ -37,7 +38,6 @@ public class MessageConverter {
   }
 
   public static DeadMessage toDeadMessage(Message message, Subscription subscription, MessageSendingResult messageSendingResult) {
-     if (messageSendingResult.getRootCause() != null) {
        return new DeadMessage(
                message.getId(),
                message.getOffset(),
@@ -49,30 +49,8 @@ public class MessageConverter {
                message.getPublishingTimestamp(),
                message.getReadingTimestamp(),
                message.getData(),
-               messageSendingResult.getRootCause()
+               messageSendingResult.toString()
        );
-     } else {
-       StringBuilder cause= new StringBuilder(String.format("Status code : %s", messageSendingResult.getStatusCode()));
-       if (messageSendingResult.getLogInfo() != null) {
-          messageSendingResult.getLogInfo().stream()
-                  .filter(logInfo -> logInfo.getRootCause() != null)
-                  .forEach( logInfo -> cause.append(String.format("\n%s, %s", logInfo.getRootCause(), logInfo))
-          );
-       }
-       return new DeadMessage(
-               message.getId(),
-               message.getOffset(),
-               message.getPartition(),
-               message.getPartitionAssignmentTerm(),
-               message.getTopic(),
-               subscription.getName(),
-               message.getKafkaTopic().asString(),
-               message.getPublishingTimestamp(),
-               message.getReadingTimestamp(),
-               message.getData(),
-               cause.toString()
-       );
-     }
   }
 
 }
