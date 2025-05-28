73de3c92603e25e255efaf5ae746038faf6d6d75
mateusz.dobrowolski <mateusz.dobrowolski@allegro.com>
2025-05-28 17:20:29 +0200

    GH-1808 | Enable conversions on preview

--- hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/publishing/message/MessageToJsonConverter.java
+++ hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/publishing/message/MessageToJsonConverter.java
@@ -2,15 +2,23 @@ package pl.allegro.tech.hermes.frontend.publishing.message;
 
 import static pl.allegro.tech.hermes.common.message.converter.AvroRecordToBytesConverter.bytesToRecord;
 
+import java.util.List;
+import org.apache.avro.Conversion;
 import org.apache.avro.Schema;
 import pl.allegro.tech.hermes.common.message.wrapper.SchemaAwareSerDe;
 import pl.allegro.tech.hermes.schema.CompiledSchema;
 import tech.allegro.schema.json2avro.converter.AvroJsonConverter;
+import tech.allegro.schema.json2avro.converter.conversions.DecimalAsStringConversion;
 
 public class MessageToJsonConverter {
-  private final AvroJsonConverter converter = new AvroJsonConverter();
+  private final List<Conversion<?>> defaultConversions = List.of(DecimalAsStringConversion.INSTANCE);
+  private final AvroJsonConverter converter;
 
-  public byte[] convert(Message message, boolean schemaIdAwareSerializationEnabled) {
+    public MessageToJsonConverter() {
+        this.converter = new AvroJsonConverter(defaultConversions.toArray(new Conversion[0]));
+    }
+
+    public byte[] convert(Message message, boolean schemaIdAwareSerializationEnabled) {
     try {
       return message
           .<Schema>getCompiledSchema()
@@ -28,6 +36,6 @@ public class MessageToJsonConverter {
         schemaIdAwareSerializationEnabled
             ? SchemaAwareSerDe.trimMagicByteAndSchemaVersion(avro)
             : avro;
-    return converter.convertToJson(bytesToRecord(schemaAwareAvro, schema.getSchema()));
+    return converter.convertToJson(bytesToRecord(schemaAwareAvro, schema.getSchema(), defaultConversions));
   }
 }
