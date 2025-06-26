23000d36ec9646afe2dbb93eb2428dd6a3b51d71
MateuszDobrowolski <88532138+MateuszDobrowolski@users.noreply.github.com>
2025-06-26 15:45:49 +0200

    GH-1808 | Logical type decimal to json as string (#1957)
    * GH-1808 | Logical type decimal to json as string
    
    * GH-1808 | Reformat code
    
    * GH-1808 | Add default list of conversions
    
    * GH-1808 | Enable conversions on preview
    
    * GH-1808 | Fix formatting
    
    * GH-1808 | Add missing file
    
    * GH-1808 | Add snapshot version from maven
    
    * GH-1808 | Switch to central sonatype
    
    * GH-1808 | Logical type decimal to json as string
    
    * GH-1808 | Reformat code
    
    * GH-1808 | Add default list of conversions
    
    * GH-1808 | Enable conversions on preview
    
    * GH-1808 | Fix formatting
    
    * GH-1808 | Add missing file
    
    * GH-1808 | Add snapshot version from maven
    
    * GH-1808 | Switch to central sonatype
    
    * Remove duplicated sonatypes links
    
    * GH-1808 | Use release version
    
    ---------
    
    Co-authored-by: Daniel Fąderski <daniel.faderski@allegro.pl>

--- hermes-common/src/main/java/pl/allegro/tech/hermes/common/message/converter/AvroRecordToBytesConverter.java
+++ hermes-common/src/main/java/pl/allegro/tech/hermes/common/message/converter/AvroRecordToBytesConverter.java
@@ -2,6 +2,8 @@ package pl.allegro.tech.hermes.common.message.converter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import org.apache.avro.Conversion;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
@@ -12,6 +14,11 @@ public interface AvroRecordToBytesConverter {
 
   static GenericRecord bytesToRecord(byte[] data, Schema schema) {
     return AvroBinaryDecoders.decodeReusingThreadLocalBinaryDecoder(data, schema);
+  }
+
+  static GenericRecord bytesToRecord(
+      byte[] data, Schema schema, Collection<Conversion<?>> conversions) {
+    return AvroBinaryDecoders.decodeReusingThreadLocalBinaryDecoder(data, schema, conversions);
   }
 
   static byte[] recordToBytes(GenericRecord genericRecord, Schema schema) throws IOException {
