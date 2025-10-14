a78342e39cb06be143d0eaba0bdf9316774de28e
Adam <adam.szorcz@allegro.pl>
2025-10-14 15:08:13 +0200

    TANGO-2508 : Added missing configuration properties attribute

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/config/GoogleBigQueryThreadPoolProperties.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/config/GoogleBigQueryThreadPoolProperties.java
@@ -1,5 +1,8 @@
 package pl.allegro.tech.hermes.consumers.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "consumer.google.bigquery.thread-pool")
 public class GoogleBigQueryThreadPoolProperties {
   int poolSize = 100;
 
