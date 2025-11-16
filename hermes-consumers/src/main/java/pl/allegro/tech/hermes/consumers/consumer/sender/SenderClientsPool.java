a006bf39b35f8e7d63322b10d30dec7b073b217c
Adam <adam.szorcz@allegro.pl>
2025-11-16 15:30:42 +0100

    TANGO-2508 : Brought back SenderClientsPool

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/SenderClientsPool.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/SenderClientsPool.java
@@ -1,47 +1,46 @@
 package pl.allegro.tech.hermes.consumers.consumer.sender;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Pool of sender clients. It creates and keeps clients for different targets. It is thread-safe.
- *
- * @param <T> type of the target
- * @param <C> type of the client
- */
 public abstract class SenderClientsPool<T extends SenderTarget, C extends SenderClient> {
 
   private static final Logger logger = LoggerFactory.getLogger(SenderClientsPool.class);
 
-  private final Map<T, C> clients = new ConcurrentHashMap<T, C>();
+  private final Map<T, C> clients = new HashMap<>();
+  private final Map<T, Integer> counters = new HashMap<>();
 
-  public C acquire(T resolvedTarget) throws IOException {
-    return clients.computeIfAbsent(
-        resolvedTarget,
-        key -> {
-          try {
-            return createClient(key);
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        });
+  public synchronized C acquire(T resolvedTarget) throws IOException {
+    C client = clients.get(resolvedTarget);
+    if (client == null) {
+      client = createClient(resolvedTarget);
+    }
+    clients.put(resolvedTarget, client);
+    Integer counter = counters.getOrDefault(resolvedTarget, 0);
+    counters.put(resolvedTarget, ++counter);
+    return client;
   }
 
-  public void release(T resolvedTarget) {
-    C client = clients.remove(resolvedTarget);
-    if (client != null) {
-      client.shutdown();
-    } else {
+  public synchronized void release(T resolvedTarget) {
+    Integer counter = counters.getOrDefault(resolvedTarget, 0);
+    if (counter == 0) {
       logger.warn("Attempt to release client that is not acquired");
+    } else if (counter == 1) {
+      counters.remove(resolvedTarget);
+      C client = clients.remove(resolvedTarget);
+      client.shutdown();
+    } else if (counter > 1) {
+      counters.put(resolvedTarget, --counter);
     }
   }
 
   public synchronized void shutdown() {
     clients.values().forEach(SenderClient::shutdown);
     clients.clear();
+    counters.clear();
   }
 
   protected abstract C createClient(T resolvedTarget) throws IOException;
