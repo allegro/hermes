3ffab788753b5eda6972238e38babb405a4e1e89
adrian.stypinski <adrian.stypinski@allegro.com>
2025-11-18 15:51:13 +0100

    PR fixes

--- hermes-console/src/components/tracking-card/TrackingCard.spec.ts
+++ hermes-console/src/components/tracking-card/TrackingCard.spec.ts
@@ -25,10 +25,22 @@ describe('TrackingCard', () => {
 
     // then
     const elements = container.querySelectorAll('a')!!;
+
+    // first list item has link
     expect(elements[0]).toHaveAttribute('href', 'https://test-tracking-url1');
     expect(elements[0]).toHaveTextContent('url1');
+
+    // link inside the first list item has link
+    expect(elements[1]).toHaveTextContent('url1');
+    expect(elements[1]).toHaveAttribute('href', 'https://test-tracking-url1');
+
+    // first list item has link
     expect(elements[2]).toHaveAttribute('href', 'https://test-tracking-url2');
     expect(elements[2]).toHaveTextContent('url2');
+
+    // link inside the second list item has link
+    expect(elements[3]).toHaveTextContent('url2');
+    expect(elements[3]).toHaveAttribute('href', 'https://test-tracking-url2');
   });
 
   it('should render message when no tracking urls', () => {
