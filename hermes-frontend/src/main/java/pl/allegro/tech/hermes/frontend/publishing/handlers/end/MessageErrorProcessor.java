c3624d29224ca3b5f5fa260542692b97285575dd
Michał Dąbrowski <michal.dabrowski@allegro.com>
2026-02-19 12:32:15 +0100

    Adjust logging levels

--- hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/publishing/handlers/end/MessageErrorProcessor.java
+++ hermes-frontend/src/main/java/pl/allegro/tech/hermes/frontend/publishing/handlers/end/MessageErrorProcessor.java
@@ -14,9 +14,12 @@ import jakarta.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.slf4j.spi.LoggingEventBuilder;
+import pl.allegro.tech.hermes.api.ErrorCode;
 import pl.allegro.tech.hermes.api.ErrorDescription;
 import pl.allegro.tech.hermes.api.Topic;
 import pl.allegro.tech.hermes.frontend.publishing.handlers.AttachmentContent;
@@ -24,6 +27,9 @@ import pl.allegro.tech.hermes.tracker.frontend.Trackers;
 
 public class MessageErrorProcessor {
   private static final Logger logger = LoggerFactory.getLogger(MessageErrorProcessor.class);
+  private static final Set<ErrorCode> CLIENT_ERRORS =
+      Set.of(ErrorCode.VALIDATION_ERROR, ErrorCode.THROUGHPUT_QUOTA_VIOLATION, ErrorCode.TIMEOUT);
+
   private final ObjectMapper objectMapper;
   private final Trackers trackers;
   private final HttpString messageIdHeader = new HttpString(MESSAGE_ID.getName());
@@ -46,6 +52,8 @@ public class MessageErrorProcessor {
         topic,
         messageId,
         readHostAndPort(exchange),
+        levelFor(error),
+        null,
         trackingHeadersExtractor.extractHeadersToLog(exchange.getRequestHeaders()));
   }
 
@@ -61,6 +69,7 @@ public class MessageErrorProcessor {
         topic,
         messageId,
         readHostAndPort(exchange),
+        levelFor(error),
         exception,
         trackingHeadersExtractor.extractHeadersToLog(exchange.getRequestHeaders()));
   }
@@ -73,6 +82,7 @@ public class MessageErrorProcessor {
         topic,
         messageId,
         readHostAndPort(exchange),
+        Level.ERROR,
         e,
         trackingHeadersExtractor.extractHeadersToLog(exchange.getRequestHeaders()));
   }
@@ -133,6 +143,7 @@ public class MessageErrorProcessor {
         attachment.getTopic(),
         attachment.getMessageId(),
         readHostAndPort(exchange),
+        Level.ERROR,
         exception,
         trackingHeadersExtractor.extractHeadersToLog(exchange.getRequestHeaders()));
   }
@@ -142,41 +153,27 @@ public class MessageErrorProcessor {
       Topic topic,
       String messageId,
       String hostAndPort,
+      Level level,
+      Exception exception,
       Map<String, String> extraRequestHeaders) {
-    logger
-        .atError()
-        .addKeyValue(TOPIC_NAME, topic.getQualifiedName())
-        .log(
-            "{}; publishing on topic: {}; message id: {}; remote host: {}",
-            errorMessage,
-            topic.getQualifiedName(),
-            messageId,
-            hostAndPort);
+    LoggingEventBuilder logBuilder =
+        logger.atLevel(level).addKeyValue(TOPIC_NAME, topic.getQualifiedName());
+    if (exception != null) {
+      logBuilder = logBuilder.setCause(exception);
+    }
+    logBuilder.log(
+        "{}; publishing on topic: {}; message id: {}; remote host: {}",
+        errorMessage,
+        topic.getQualifiedName(),
+        messageId,
+        hostAndPort);
     trackers
         .get(topic)
         .logError(messageId, topic.getName(), errorMessage, hostAndPort, extraRequestHeaders);
   }
 
-  private void log(
-      String errorMessage,
-      Topic topic,
-      String messageId,
-      String hostAndPort,
-      Exception exception,
-      Map<String, String> extraRequestHeaders) {
-    logger
-        .atError()
-        .addKeyValue(TOPIC_NAME, topic.getQualifiedName())
-        .setCause(exception)
-        .log(
-            "{}; publishing on topic: {}; message id: {}; remote host: {}",
-            errorMessage,
-            topic.getQualifiedName(),
-            messageId,
-            hostAndPort);
-    trackers
-        .get(topic)
-        .logError(messageId, topic.getName(), errorMessage, hostAndPort, extraRequestHeaders);
+  private Level levelFor(ErrorDescription error) {
+    return CLIENT_ERRORS.contains(error.getCode()) ? Level.WARN : Level.ERROR;
   }
 
   private void send(HttpServerExchange exchange, ErrorDescription error, String messageId)
