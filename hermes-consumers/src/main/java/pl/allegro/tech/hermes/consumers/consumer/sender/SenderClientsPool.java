4d11d171bc7cd0b3e65c9e5c7b7ec79d3e695bce
Adam <adam.szorcz@allegro.pl>
2026-03-17 14:02:28 +0100

    TANGO-2997 : Changed condition

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/SenderClientsPool.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/SenderClientsPool.java
@@ -48,7 +48,7 @@ public abstract class SenderClientsPool<T extends SenderTarget, C extends Sender
   public synchronized void remove(T resolvedTarget) {
     Integer counter = counters.getOrDefault(resolvedTarget, 0);
     LocalDateTime lastReleaseDate = lastReleaseAllDate.get(resolvedTarget);
-    if (lastReleaseDate != null && lastReleaseDate.plusSeconds(30).isBefore(LocalDateTime.now())) {
+    if (lastReleaseDate == null || lastReleaseDate.plusSeconds(30).isBefore(LocalDateTime.now())) {
       if (counter > 0) {
         logger.info(
             "Releasing all clients for target {}. Current counter: {}", resolvedTarget, counter);
