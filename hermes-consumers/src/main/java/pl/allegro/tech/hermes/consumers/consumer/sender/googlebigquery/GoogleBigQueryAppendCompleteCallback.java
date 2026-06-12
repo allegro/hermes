65c847362a0c06bbb82367b87c65159a4175a005
Adam <adam.szorcz@allegro.pl>
2026-06-12 14:29:53 +0200

    TANGO-3103 : Reformatted code v2

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/googlebigquery/GoogleBigQueryAppendCompleteCallback.java
+++ hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/sender/googlebigquery/GoogleBigQueryAppendCompleteCallback.java
@@ -20,11 +20,14 @@ public class GoogleBigQueryAppendCompleteCallback implements ApiFutureCallback<A
   public static Integer mapToPermanentErrorHttpStatus(Throwable cause) {
     Status.Code grpcCode = Status.fromThrowable(cause).getCode();
     return switch (grpcCode) {
-        case NOT_FOUND -> 404; // Table does not exist
-        case PERMISSION_DENIED -> 403; // Technical user does not have permissions to write to the table
-        case INVALID_ARGUMENT -> 400; // Invalid message format i.e. microsecond timestamp value is sent to millisecond timestamp field
-        default -> 500;
-      };
+      case NOT_FOUND -> 404; // Table does not exist
+      case PERMISSION_DENIED ->
+          403; // Technical user does not have permissions to write to the table
+      case INVALID_ARGUMENT ->
+          400; // Invalid message format i.e. microsecond timestamp value is sent to millisecond
+               // timestamp field
+      default -> 500;
+    };
   }
 
   @Override
