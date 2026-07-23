d6c7b2c124345e0a6bd4f11c8cb92ad4c3a8fb74
Mateusz <76775507+szczygiel-m@users.noreply.github.com>
2026-07-23 15:01:54 +0200

    Removing endpoint interpolation mechanism (#2049)

--- hermes-consumers/src/main/java/pl/allegro/tech/hermes/consumers/consumer/interpolation/UriInterpolator.java
+++
@@ -1,9 +0,0 @@
-package pl.allegro.tech.hermes.consumers.consumer.interpolation;
-
-import java.net.URI;
-import pl.allegro.tech.hermes.api.EndpointAddress;
-import pl.allegro.tech.hermes.consumers.consumer.Message;
-
-public interface UriInterpolator {
-  URI interpolate(EndpointAddress endpoint, Message message) throws InterpolationException;
-}
